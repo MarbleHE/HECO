ba9b4b9af77e2f7dfe68a19fb28c21bb96f148a2
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-01-31 10:07:09 +0100

    Reverted main class to demo example

--- main.cpp
+++ main.cpp
@@ -1,73 +1,9 @@
+#include <genAstDemo.h>
 #include "main.h"
 
 
-void buildMultDepthRewritingCircuitSample1(Ast &ast) {
-    Function *f = new Function("demoCkt");
-    f->addStatement(new Return(new LogicalExpr(
-            new LogicalExpr(
-                    new LogicalExpr(
-                            new LogicalExpr(
-                                    new LogicalExpr(
-                                            new Variable("a_1^(1)_left"),
-                                            OpSymb::logicalAnd,
-                                            new Variable("a_1^(1)_right")),
-                                    OpSymb::logicalAnd,
-                                    new LogicalExpr(
-                                            new Variable("a_2^(1)_left"),
-                                            OpSymb::logicalXor,
-                                            new Variable("a_2^(1)_right"))),
-                            OpSymb::logicalXor,
-                            new LogicalExpr(
-                                    new LogicalExpr(
-                                            new Variable("a_1^(2)_left"),
-                                            OpSymb::logicalAnd,
-                                            new Variable("a_1^(2)_right")),
-                                    OpSymb::logicalAnd,
-                                    new LogicalExpr(
-                                            new Variable("a_2^(2)_left"),
-                                            OpSymb::logicalXor,
-                                            new Variable("a_2^(2)_right")))),
-                    OpSymb::logicalXor,
-                    new Variable("y_1")),
-            OpSymb::logicalAnd,
-            new Variable("a_t"))));
-    ast.setRootNode(f);
-}
-
-void buildMultDepthRewritingCircuitSample0(Ast &ast) {
-    Function *f = new Function("demoCkt");
-    f->addStatement(new Return(new LogicalExpr(
-            new LogicalExpr(
-                    new LogicalExpr(
-                            new LogicalExpr(
-                                    new Variable("a_1^(1)"),
-                                    OpSymb::logicalAnd,
-                                    new Variable("a_2^(1)")),
-                            OpSymb::logicalXor,
-                            new LogicalExpr(
-                                    new Variable("a_1^(2)"),
-                                    OpSymb::logicalAnd,
-                                    new Variable("a_2^(2)"))),
-                    OpSymb::logicalXor,
-                    new Variable("y_1")),
-            OpSymb::logicalAnd,
-            new Variable("a_t"))));
-    ast.setRootNode(f);
-}
-
-
 int main() {
-    //runInteractiveDemo();
-
-    // build demo circuit
-    Ast ast;
-
-    buildMultDepthRewritingCircuitSample1(ast);
-
-
-    // apply cone rewriting to AST
-    ConeRewriter::applyConeRewriting(ast);
-
+    runInteractiveDemo();
     return 0;
 }
 
