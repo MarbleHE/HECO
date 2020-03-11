e17e844b919cbb4f996f59671b497ce9bb4a628b
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-03-11 17:46:49 +0100

    Fixes the DotPrinterTest causing issues by Github actions

--- test/DotPrinterTest.cpp
+++ test/DotPrinterTest.cpp
@@ -145,8 +145,6 @@ TEST_F(DotPrinterFixture,
       .setOutputStream(outputStream);
   dp.printAsDotFormattedGraph(ast);
 
-  std::cout << outputStream.str() << std::endl;
-
   // read expected output from file
   std::ifstream ifs("../../test/expected_output_large/DotPrinterTest/"
                     "printAsDotFormattedGraphTest_printAstExample1.txt");
