d1f2ad5a5e1951d1bb8be1bf1c84f2219bd35036
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-03-13 16:39:10 +0100

    Added AbstractNode inheritance to AbstractMatrix, moved comments from template Matrix<T> to AbstractMatrix

--- include/ast/AbstractMatrix.h
+++ include/ast/AbstractMatrix.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <nlohmann/json.hpp>
+#include "AbstractNode.h"
 
 class Dimension;
 class AbstractLiteral;
@@ -16,25 +17,71 @@ using json = nlohmann::json;
 
 /// A helper class that allows to access and use all Matrix<T> instances using a unified interface, without
 /// needing exact knowledge about the specific template type T.
-class AbstractMatrix {
+class AbstractMatrix : public AbstractNode {
  public:
+  /// Rotates a matrix by a given rotationFactor.
+  /// \param rotationFactor Determines the number of rotations and its direction. If rotationFactor is positive (>0),
+  ///        then the elements are rotated to the right, otherwise (rotationFactor<0) the elements are rotated to the
+  ///        left.
+  /// \param inPlace If True, then modifies the current matrix and returns the modified matrix. In case that
+  ///        inPlace is False, keeps the current matrix untouched and returns instead a transposed copy.
+  /// \return The rotated matrix that is either the rotated current matrix (inPlace=True) or a rotated copy of
+  ///         the current matrix (inPlace=False).
   virtual AbstractMatrix *rotate(int rotationFactor, bool inPlace) = 0;
 
+  /// Transposes a matrix, i.e., moves every element a_ij to a_ji.
+  /// \param inPlace If True, then modifies the current matrix and returns the modified matrix. In case that
+  ///        inPlace is False, keeps the current matrix untouched and returns instead a transposed copy.
+  /// \return The transposed matrix that is either the transposed current matrix (inPlace=True) or a transposed copy of
+  ///         the current matrix (inPlace=False).
   virtual AbstractMatrix *transpose(bool inPlace) = 0;
 
+  /// Returns the dimension object that indicates the matrix dimensions.
+  /// \return A reference to the dimension object associated to this matrix.
   virtual Dimension &getDimensions() = 0;
 
+  /// Creates a string representation of this matrix using the matrix text representation by MATLAB.
+  /// For example, a 3x2 matrix would look like [2 3; 2 332; 43 3] where the semicolon (;) serves as row delimiter.
+  /// \return A string representation of this matrix.
   virtual std::string toString() = 0;
 
+  /// Checks whether this matrix is a scalar, i.e., has dimension (1,1).
+  /// \return True if this matrix is a scalar, otherwise False.
   [[nodiscard]] virtual bool isScalar() const = 0;
 
-  [[nodiscard]] virtual json toJson() const = 0;
+  /// Creates a JSON representation of this matrix.
+  /// \return The JSON representation of this matrix.
+  [[nodiscard]] json toJson() const override = 0;
 
+  /// A helper method that is defined in AbstractMatrix and overridden here. It casts the first parameter
+  /// (AbstractMatrix) to Matrix<T> where T is the template type (e.g., int, float). This is needed to call the suitable
+  /// applyBinaryOperatorComponentwise method that handles the binary operator application.
+  /// \param rhsOperand The operand on the right hand-side. The current matrix is the left hand-side operand.
+  /// \param op The operator to be applied on the two given matrices.
+  /// \return The AbstractMatrix resulting from applying the operator op on the two matrices.
   virtual AbstractMatrix *applyBinaryOperator(AbstractMatrix *rhsOperand, Operator *os) = 0;
 
+  /// Applies the unary operator specified by the given Operator os to every element of the matrix.
+  /// \param os The operator to be applied to every element of the matrix.
+  /// \return A new matrix where the Operator as was applied to every element.
   virtual AbstractMatrix *applyUnaryOperatorComponentwise(Operator *os) = 0;
 
+  /// Returns an AbstractExpr containing the value of the element at index specified by the given (row, column) pair.
+  /// The value is an AbstractExpr pointer because matrix elements can be defined using expressions.
+  /// \param row The row number of the element to be returned as a pointer.
+  /// \param column The column number of the element to be returned as a pointer.
+  /// \return An AbstractExpr pointer pointing to the value of the element at position (row, column).
   virtual AbstractExpr *getElementAt(int row, int column) = 0;
+
+  ///
+  /// \param row
+  /// \param column
+  /// \param ae
+  virtual void setElementAt(int row, int column, AbstractExpr *ae) = 0;
+
+  ///
+  /// \return
+  virtual bool containsAbstractExprs() = 0;
 
   bool operator==(const AbstractMatrix &rhs) const;
 
--- include/ast/Matrix.h
+++ include/ast/Matrix.h
@@ -167,8 +167,6 @@ class Matrix : public AbstractMatrix {
     values = other.values;
   }
 
-  /// Checks whether this matrix is a scalar, i.e., has dimension (1,1).
-  /// \return True if this matrix is a scalar, otherwise False.
   [[nodiscard]] bool isScalar() const override {
     return dim.equals(1, 1);
   }
@@ -190,8 +188,6 @@ class Matrix : public AbstractMatrix {
     return !(rhs==*this);
   }
 
-  /// Creates a JSON representation of this matrix.
-  /// \return The JSON representation of this matrix.
   [[nodiscard]] json toJson() const override;
 
   ///
@@ -217,15 +213,12 @@ class Matrix : public AbstractMatrix {
     return values[row][column];
   }
 
-  /// Returns an AbstractExpr containing the value of the element at index specified by the given (row, column) pair.
-  /// The value is an AbstractExpr pointer because matrix elements can be defined using expressions.
-  /// \param row The row number of the element to be returned as a pointer.
-  /// \param column The column number of the element to be returned as a pointer.
-  /// \return An AbstractExpr pointer pointing to the value of the element at position (row, column).
   AbstractExpr *getElementAt(int row, int column) override;
 
-  /// Returns the dimension object that indicates the matrix dimensions.
-  /// \return A reference to the dimension object associated to this matrix.
+  void setElementAt(int row, int column, AbstractExpr *element) override;
+
+  void replaceChild(AbstractNode *originalChild, AbstractNode *newChildToBeAdded) override;
+
   Dimension &getDimensions() override {
     return dim;
   }
@@ -236,9 +229,10 @@ class Matrix : public AbstractMatrix {
     values = newValues;
   }
 
-  /// Creates a string representation of this matrix using the matrix text representation by MATLAB.
-  /// For example, a 3x2 matrix would look like [2 3; 2 332; 43 3] where the semicolon (;) serves as row delimiter.
-  /// \return A string representation of this matrix.
+  void addElementToStringStream(T elem, std::stringstream &s);
+
+  bool containsAbstractExprs() override;
+
   std::string toString() override {
     std::stringstream outputStr;
     // print boolean values as text (true, false) by default, otherwise the output (0,1) cannot be distinguished from
@@ -255,7 +249,8 @@ class Matrix : public AbstractMatrix {
     outputStr << "[";
     for (int i = 0; i < values.size(); ++i) {
       for (int j = 0; j < values[i].size(); ++j) {
-        outputStr << values[i][j];
+//        outputStr << values[i][j];
+        addElementToStringStream(values[i][j], outputStr);
         if (j!=values[i].size() - 1) outputStr << elementDelimiter;
       }
       if (i!=values.size() - 1) outputStr << rowDelimiter;
@@ -277,11 +272,6 @@ class Matrix : public AbstractMatrix {
     return true;
   }
 
-  /// Transposes a matrix, i.e., moves every element a_ij to a_ji.
-  /// \param inPlace If True, then modifies the current matrix and returns the modified matrix. In case that
-  ///        inPlace is False, keeps the current matrix untouched and returns instead a transposed copy.
-  /// \return The transposed matrix that is either the transposed current matrix (inPlace=True) or a transposed copy of
-  ///         the current matrix (inPlace=False).
   Matrix<T> *transpose(bool inPlace) override {
     Matrix<T> *matrixToTranspose = inPlace ? this : new Matrix<T>(*this);
     std::vector<std::vector<T>> transposedVec(matrixToTranspose->values[0].size(), std::vector<T>());
@@ -295,14 +285,6 @@ class Matrix : public AbstractMatrix {
     return matrixToTranspose;
   }
 
-  /// Rotates a matrix by a given rotationFactor.
-  /// \param rotationFactor Determines the number of rotations and its direction. If rotationFactor is positive (>0),
-  ///        then the elements are rotated to the right, otherwise (rotationFactor<0) the elements are rotated to the
-  ///        left.
-  /// \param inPlace If True, then modifies the current matrix and returns the modified matrix. In case that
-  ///        inPlace is False, keeps the current matrix untouched and returns instead a transposed copy.
-  /// \return The rotated matrix that is either the rotated current matrix (inPlace=True) or a rotated copy of
-  ///         the current matrix (inPlace=False).
   Matrix<T> *rotate(int rotationFactor, bool inPlace) override {
     Matrix<T> *matrixToRotate = inPlace ? this : new Matrix<T>(*this);
     if (matrixToRotate->getDimensions().equals(1, -1)) {  // a row vector
@@ -339,12 +321,6 @@ class Matrix : public AbstractMatrix {
     return new Matrix<T>(*this);
   }
 
-  /// A helper method that is defined in AbstractMatrix and overridden here. It casts the first parameter
-  /// (AbstractMatrix) to Matrix<T> where T is the template type (e.g., int, float). This is needed to call the suitable
-  /// applyBinaryOperatorComponentwise method that handles the binary operator application.
-  /// \param rhsOperand The operand on the right hand-side. The current matrix is the left hand-side operand.
-  /// \param op The operator to be applied on the two given matrices.
-  /// \return The AbstractMatrix resulting from applying the operator op on the two matrices.
   AbstractMatrix *applyBinaryOperator(AbstractMatrix *rhsOperand, Operator *op) override {
     return applyBinaryOperatorComponentwise(dynamic_cast<Matrix<T> *>(rhsOperand), op);
   }
@@ -355,10 +331,17 @@ class Matrix : public AbstractMatrix {
   /// \return AbstractMatrix resulting from applying the operator op on the two matrices.
   AbstractMatrix *applyBinaryOperatorComponentwise(Matrix<T> *rhsOperand, Operator *op);
 
-  /// Applies the unary operator specified by the given Operator os to every element of the matrix.
-  /// \param os The operator to be applied to every element of the matrix.
-  /// \return A new matrix where the Operator as was applied to every element.
   AbstractMatrix *applyUnaryOperatorComponentwise(Operator *os) override;
+
+  [[nodiscard]] std::string getNodeType() const override;
+
+  void accept(Visitor &v) override;
+
+  AbstractNode *clone(bool keepOriginalUniqueNodeId) override;
+
+  int getMaxNumberChildren() override;
+
+  bool supportsCircuitMode() override;
 };
 
 #endif //AST_OPTIMIZER_INCLUDE_AST_MATRIX_H_
