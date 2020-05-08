763a8094fd95ccc4ae37e5e5a8de1affe04d443c
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-05-09 00:32:59 +0200

    Adds constructors to plaintext class

--- include/ast_opt/mockup_classes/Plaintext.h
+++ include/ast_opt/mockup_classes/Plaintext.h
@@ -2,8 +2,14 @@
 #define AST_OPTIMIZER_SRC_MOCKUP_CLASSES_PLAINTEXT_H_
 
 #include "Ciphertext.h"
-class Plaintext : Ciphertext {
 
+class Plaintext : public Ciphertext {
+ public:
+  explicit Plaintext(const Ciphertext &ctxt);
+
+  explicit Plaintext(double scalar, int numCiphertextSlots = Ciphertext::DEFAULT_NUM_SLOTS);
+
+  explicit Plaintext(const std::vector<double> &data, int numCiphertextSlots = Ciphertext::DEFAULT_NUM_SLOTS);
 };
 
 #endif //AST_OPTIMIZER_SRC_MOCKUP_CLASSES_PLAINTEXT_H_
--- src/mockup_classes/Plaintext.cpp
+++ src/mockup_classes/Plaintext.cpp
@@ -1,1 +1,7 @@
 #include "ast_opt/mockup_classes/Plaintext.h"
+
+Plaintext::Plaintext(const Ciphertext &ctxt) : Ciphertext(ctxt) {}
+
+Plaintext::Plaintext(double scalar, int numCiphertextSlots) : Ciphertext(scalar, numCiphertextSlots) {}
+
+Plaintext::Plaintext(const std::vector<double> &data, int numCiphertextSlots) : Ciphertext(data, numCiphertextSlots) {}
