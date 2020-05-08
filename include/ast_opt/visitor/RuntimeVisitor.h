8982394472d9b0e670280e77878d1c256a6eb530
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-05-08 07:21:26 +0200

    WIP RuntimeVisitor: Added rotation logic
    Added the logic that determines the required rotations of a ciphertext, finds the optimal rotations based on the existing ciphertexts and performs the rotation on the Ciphertext objects.

--- include/ast_opt/visitor/RuntimeVisitor.h
+++ include/ast_opt/visitor/RuntimeVisitor.h
@@ -3,17 +3,43 @@
 
 #include "Visitor.h"
 #include <map>
+#include "ast_opt/mockup_classes/Ciphertext.h"
+#include "ast_opt/ast/Matrix.h"
 
 class EvaluationVisitor;
+class Ciphertext;
+class AbstractMatrix;
+
+/*
+ * A helper struct to represent the return value of determineRequiredRotations that determines the required
+ * rotations for ciphertexts.
+ */
+struct RotationData {
+  int targetIndex;
+  Ciphertext baseCiphertext;
+  int requiredNumRotations;
+
+  RotationData(int targetIndex, const Ciphertext &baseCiphertext, int requiredNumRotations)
+      : targetIndex(targetIndex), baseCiphertext(baseCiphertext), requiredNumRotations(requiredNumRotations) {}
+};
+
 class RuntimeVisitor : public Visitor {
  private:
+  // an instance of the EvaluationVisitor to be used to partially evaluate certain parts/expressions of the AST
   EvaluationVisitor *ev;
 
+  // an enum that defines valid variable access types
   enum MatrixAccessMode { READ = 0, WRITE = 1 };
+
+  // the access mode that is used for new entries in the variableAccessMap
   MatrixAccessMode currentMatrixAccessMode = READ;
 
+  // tracks accessed matrix indices
   std::map<std::string,
-           std::map<std::pair<int, int>, MatrixAccessMode>> variableAccessMap;
+           std::map<std::pair<int, int>, MatrixAccessMode>> matrixAccessMap;
+
+  // stores for each variable (string) all created ciphertexts including rotated variants with their offset (int)
+  std::map<std::string, std::map<int, Ciphertext>> varValues;
 
  public:
   explicit RuntimeVisitor(std::unordered_map<std::string, AbstractLiteral *> funcCallParameterValues);
@@ -26,10 +52,37 @@ class RuntimeVisitor : public Visitor {
 
   void visit(MatrixAssignm &elem) override;
 
-  void registerMatrixAccess(std::string variableIdentifier, int rowIndex, int columnIndex);
+  void registerMatrixAccess(const std::string &variableIdentifier, int rowIndex, int columnIndex);
 
   void visit(VarDecl &elem) override;
+
+  void visit(FunctionParameter &elem) override;
+
+  void visit(Datatype &elem) override;
+
+  static void checkIndexValidity(std::pair<int, int> rowColumnIndex);
+
+  static std::vector<RotationData> determineRequiredRotations(std::map<int, Ciphertext> existingRotations,
+                                                       const std::set<int>& reqIndices,
+                                                       int targetSlot = -1);
+
+  void executeRotations(const std::string &varIdentifier, const std::vector<RotationData> &reqRotations);
+  void visit(OperatorExpr &elem) override;
 };
+
+template<typename T>
+std::vector<double> transformToDoubleVector(AbstractMatrix *mx) {
+  auto matrix = dynamic_cast<Matrix<T> *>(mx);
+  std::vector<T> rawVector;
+  if (matrix->getDimensions().equals(1, -1)) {
+    rawVector = matrix->getNthRowVector(0);
+  } else if (matrix->getDimensions().equals(-1, 1)) {
+    rawVector = matrix->getNthColumnVector(0);
+  } else {
+    throw std::runtime_error("Given AbstractMatrix is neither a row (1,x) nor a column (x,1) vector!");
+  }
+  return std::vector<double>(rawVector.begin(), rawVector.end());
+}
 
 #endif //AST_OPTIMIZER_INCLUDE_AST_OPT_VISITOR_RUNTIMEVISITOR_H_
 
--- src/visitor/RuntimeVisitor.cpp
+++ src/visitor/RuntimeVisitor.cpp
@@ -7,15 +7,15 @@
 #include "ast_opt/ast/Variable.h"
 #include "ast_opt/ast/VarDecl.h"
 #include "ast_opt/ast/MatrixAssignm.h"
+#include "ast_opt/ast/FunctionParameter.h"
+#include "ast_opt/mockup_classes/Ciphertext.h"
 
 void RuntimeVisitor::visit(Ast &elem) {
   Visitor::visit(elem);
 }
 
 void RuntimeVisitor::visit(For &elem) {
-  // execute initializer
-  elem.getInitializer()->accept(*ev);
-
+  // inlines Visitor::visit
   auto conditionIsTrue = [&]() -> bool {
     // visit the condition's expression
     elem.getCondition()->accept(*ev);
@@ -24,18 +24,8 @@ void RuntimeVisitor::visit(For &elem) {
     return cond==LiteralBool(true);
   };
 
-  // while condition evaluates true
-  while (conditionIsTrue()) {
-    // execute loop body
-    elem.getStatementToBeExecuted()->accept(*ev);
+  for (elem.getInitializer()->accept(*ev); conditionIsTrue(); elem.getUpdateStatement()->accept(*ev)) {
     elem.getStatementToBeExecuted()->accept(*this);
-
-    // extract index pairs that have been accessed in this loop iteration and store them globally
-
-
-    // execute update statement
-    elem.getUpdateStatement()->accept(*ev);
-    std::cout << std::endl;
   }
 }
 
@@ -45,9 +35,8 @@ void RuntimeVisitor::visit(MatrixElementRef &elem) {
   // a helper utility that either returns the value of an already existing LiteralInt (in AST) or performs evaluation
   // and then retrieves the value of the evaluation result (LiteralInt)
   auto determineIndexValue = [&](AbstractExpr *expr) {
-    auto rowIdxLiteral = dynamic_cast<LiteralInt *>(expr);
     // if index is a literal: simply return its value
-    if (rowIdxLiteral!=nullptr) return rowIdxLiteral->getValue();
+    if (auto rowIdxLiteral = dynamic_cast<LiteralInt *>(expr)) return rowIdxLiteral->getValue();
     // if row index is not a literal: evaluate expression
     expr->accept(*ev);
     auto evalResult = ev->getResults().front();
@@ -77,24 +66,202 @@ RuntimeVisitor::RuntimeVisitor(std::unordered_map<std::string, AbstractLiteral *
   ev = new EvaluationVisitor(std::move(funcCallParameterValues));
 }
 
-void RuntimeVisitor::registerMatrixAccess(std::string variableIdentifier, int rowIndex, int columnIndex) {
+void RuntimeVisitor::registerMatrixAccess(const std::string &variableIdentifier, int rowIndex, int columnIndex) {
   std::stringstream ss;
   ss << variableIdentifier << "[" << rowIndex << "]"
      << "[" << columnIndex << "]" << "\t(" << currentMatrixAccessMode << ")";
-  variableAccessMap[variableIdentifier][std::pair(rowIndex, columnIndex)] = currentMatrixAccessMode;
-  std::cout << ss.str() << std::endl;
+  matrixAccessMap[variableIdentifier][std::pair(rowIndex, columnIndex)] = currentMatrixAccessMode;
+//  std::cout << ss.str() << std::endl;
 }
 
 void RuntimeVisitor::visit(VarDecl &elem) {
+  // Note the counter-intuitive logic here: If getVarValue does *not* throw an exception then the variable has been
+  // declared before and we need to inform the user as this will cause issues.
+  try {
+    static_cast<void>(*ev->getVarValue(elem.getIdentifier()));
+    throw std::runtime_error("RuntimeVisitor and EvaluationVisitor cannot handle variable scoping yet. Please use "
+                             "unique variable identifiers to allow the RuntimeVisitor to distinguish variables.");
+  } catch (std::logic_error &e) {}
   elem.accept(*ev);
 }
 
 void RuntimeVisitor::visit(MatrixAssignm &elem) {
-  // inline Visitor::visit(elem);
+  // inlines Visitor::visit(elem);
   Visitor::addStatementToScope(elem);
+
+  // visit the right-hand side of the MatrixAssignm
   currentMatrixAccessMode = WRITE;
   elem.getAssignmTarget()->accept(*this);
+
+  // determine information about the assignment target
+  if (matrixAccessMap.size()!=1
+      || matrixAccessMap.begin()->second.size()!=1
+      || matrixAccessMap.begin()->second.begin()->second!=WRITE) {
+    throw std::runtime_error("There should be exactly one matrix write access after visiting the MatrixAssignm's "
+                             "left-hand side. Did you forget to clear the matrixAccessMap before visiting the expr?");
+  }
+  std::string varTargetIdentifier = matrixAccessMap.begin()->first;
+  std::pair targetSlot = matrixAccessMap.begin()->second.begin()->first;
+  checkIndexValidity(targetSlot);
+  matrixAccessMap.clear();
+
+  // visit the right-hand side of the MatrixAssignm
   currentMatrixAccessMode = READ;
   elem.getValue()->accept(*this);
+
+  // collect all (read) matrix accesses on the right-hand side of this assignment and execute the rotations that are
+  // required to execute them
+  for (auto &[varIdentifier, varAccessMap] : matrixAccessMap) {
+    // TODO add check that this variable (varIdentifier) refers to an encrypted variable
+    // if (variablesDatatype.at(varIdentifier) != encrypted) continue;
+
+    std::set<int> accessedIndices;
+    for (auto &[indexPair, accessMode] : varAccessMap) {
+      checkIndexValidity(indexPair);
+      accessedIndices.insert(indexPair.second);
+    }
+
+    // get rotated ciphertexts that already exist for this variable
+    auto existingRotations = varValues.at(varIdentifier);
+
+    // determine the required rotations for this computations
+    auto reqRotations = determineRequiredRotations(existingRotations, accessedIndices, targetSlot.second);
+
+    // execute rotations on Ciphertext and add them to the varValues map
+    executeRotations(varIdentifier, reqRotations);
+  }
+  matrixAccessMap.clear();
+
+  // TODO re-visit AST and perform actions using recently generated rotated Ciphertexts and the Ciphertext operations
+  std::cout << "stop";
+}
+
+void RuntimeVisitor::executeRotations(const std::string &varIdentifier, const std::vector<RotationData> &reqRotations) {
+  for (auto rd : reqRotations) {
+//    // Check if there is already an existing ciphertext with the same rotation as we need. This should never happen
+//    // as the existing rotations should have been passed to determineRequiredRotations before.
+//    if (varValues.at(varIdentifier).count(rd.baseCiphertext.getOffsetOfFirstElement()+rd.requiredNumRotations) > 0) {
+//      std::cerr << "Requested rotation already exists. This indicates that there's something wrong with your program "
+//                   "as you should have passed the existing rotations to determineRequiredRotations." << std::endl;
+//      continue; // skip executing this rotation
+//    }
+    // no need to do anything further as the requested rotation already exists
+    if (rd.requiredNumRotations==0) continue;
+
+    // execute rotation on ciphertext
+    Ciphertext rotatedCtxt = rd.baseCiphertext.rotate(rd.requiredNumRotations);
+    // store rotated ciphertext in varValues
+    varValues.at(varIdentifier).emplace(rotatedCtxt.getOffsetOfFirstElement(), rotatedCtxt);
+  }
+}
+
+std::vector<RotationData> RuntimeVisitor::determineRequiredRotations(std::map<int, Ciphertext> existingRotations,
+                                                                     const std::set<int>& reqIndices, int targetSlot) {
+  if (targetSlot==-1) {
+    // TODO: Implement logic to find the best suitable targetSlot to realize the given reqIndices.
+    throw std::logic_error("Logic to automatically determine the best targetSlot is not implemented yet! Please "
+                           "specify a targetSlot and re-run the RuntimeVisitor.");
+  }
+
+  // Note that the resulting std::set<RotationData> always contains references to the original Ciphertext objects,
+  // passed as parameter existingRotations. This is fine as our Ciphertext's rotation method does not rotate in-place
+  // but returns a rotated copy instead. This explains why there is no need to clone the ciphertext at this point.
+
+  // if only a rot-0 ctxt is given: create all rotations for reqIndices
+  std::vector<RotationData> resultSet;
+  if (existingRotations.size()==1) {
+    // make sure that the existing rotation is the 0-ciphertext, i.e., the initial (unrotated) ciphertext
+    if (existingRotations.count(0)!=1) {
+      throw std::runtime_error("There is only one ciphertext in the given map of existing rotations but that is "
+                               "not the initial ciphertext with offset zero. Cannot continue.. aborting.");
+    }
+    // create rotations for each index in reqIndices
+    for (auto idx : reqIndices) {
+      resultSet.emplace_back(idx, existingRotations.at(0), targetSlot - idx);
+    }
+  } else { // existingRotations.size() > 1, i.e., there is more than one existing rotation for this ciphertext
+    // if other rotations are existing: figure out the "cheapest" way to align ciphertexts to given target slot based
+    // on the existing rotations
+
+    // find the optimal combination of already existing rotated ciphertext + addt. rotations for each required index
+    for (auto idx : reqIndices) {
+      // a vector that describes how many additional rotations an existing ciphertext would require to align it to
+      // the target slot
+      std::vector<std::pair<int, Ciphertext>> rotations;
+
+      // determine the difference between current index and existing index for each idx in reqIndices
+      bool canReuseExistingRotation = false;
+      rotations.reserve(existingRotations.size());
+      for (auto[offset, ctxt] : existingRotations) {
+        auto numRequiredRotations = targetSlot - (offset + idx);
+        rotations.emplace_back(numRequiredRotations, ctxt);
+        if (numRequiredRotations==0) {
+          // if we can reuse an existing rotation there is no need
+          resultSet.emplace_back(idx, ctxt, numRequiredRotations);
+          canReuseExistingRotation = true;
+          break;
+        }
+      }
+
+      // TODO: Find the combination of (existing ciphertext, needed rotations) that is "cheapest".
+      //  Implement technique used in SEAL that uses bit representation and allows determining cheapest rotation.
+      //  Take the values in the set rotations and push the determine cheapest one into resultSet.
+      if (!canReuseExistingRotation) {
+        throw std::runtime_error(
+            "Not implemented: Cannot determine the optimal (ciphertext, req. rotations) set yet.");
+      }
+    }
+  }
+  return resultSet;
+}
+
+void RuntimeVisitor::checkIndexValidity(std::pair<int, int> rowColumnIndex) {
+  if (rowColumnIndex.first!=0) {
+    throw std::runtime_error("Currently, the RuntimeVisitor only supports matrix accesses of row vectors (i.e., "
+                             "[0][x]) for secret variables. Please rewrite your program to use a row-concatenated "
+                             "vector of dimension (1,x) instead.");
+  }
+}
+
+void RuntimeVisitor::visit(FunctionParameter &elem) {
+  // inline Visitor::visit(elem);
+  elem.getDatatype()->accept(*this);
+  elem.getValue()->accept(*this);
+
+  // create new Ciphertext if this FunctionParameter refers to an encrypted variable (secret input)
+  if (elem.getDatatype()->isEncrypted()) {
+    auto var = dynamic_cast<Variable *>(elem.getValue());
+    if (var==nullptr) {
+      throw std::runtime_error("FunctionParameter's value is expected to be of type Variable!");
+    }
+
+    // transform the function parameter's value (need to be passed to RuntimeVisitor's constructor) from Matrix<int>
+    // or Matrix<float> to Matrix<double> as ciphertexts currently only support Matrix<double>
+    std::vector<double> mx;
+    auto value = ev->getVarValue(var->getIdentifier());
+    if (auto lint = dynamic_cast<LiteralInt *>(value)) {
+      mx = transformToDoubleVector<int>(lint->getMatrix());
+    } else if (auto lfloat = dynamic_cast<LiteralFloat *>(value)) {
+      mx = transformToDoubleVector<float>(lfloat->getMatrix());
+    } else {
+      throw std::runtime_error("Only LiteralInt and LiteralFloat ciphertexts supported yet!");
+    }
+
+    // create and store ciphertextt in varValues map
+    Ciphertext ctxt(mx);
+    varValues[var->getIdentifier()].emplace(ctxt.getOffsetOfFirstElement(), mx);
+  }
+}
+
+void RuntimeVisitor::visit(Datatype &elem) {
+  Visitor::visit(elem);
+}
+
+void RuntimeVisitor::visit(OperatorExpr &elem) {
+  Visitor::visit(elem);
+
+  // TODO check if secret variables are involved (integrate the secret tainting visitor!)
+  //  if yes: check if we already calculated the required rotations
+  //    if yes: execute the operation on the ciphertext
 }
 
