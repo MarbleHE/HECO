1109045bb207228e2c86e5510aadcdf855ed26c5
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-09-16 15:02:50 +0200

    WIP testing framework

--- test/parser/ParserTest.cpp
+++ test/parser/ParserTest.cpp
@@ -1,4 +1,7 @@
+#include "ast_opt/ast/Assignment.h"
+#include "ast_opt/ast/BinaryExpression.h"
 #include "ast_opt/ast/Function.h"
+#include "ast_opt/ast/FunctionParameter.h"
 #include "ast_opt/ast/Literal.h"
 #include "ast_opt/ast/Return.h"
 #include "ast_opt/parser/Parser.h"
@@ -11,7 +14,12 @@ Datatype FLOAT = Datatype(Type::FLOAT);
 Datatype DOUBLE = Datatype(Type::DOUBLE);
 Datatype STRING = Datatype(Type::STRING);
 
-auto empty_params = std::vector<std::unique_ptr<FunctionParameter>>();
+bool compareAST(const AbstractNode& ast1, const AbstractNode& ast2) {
+  //TODO: Write helper function to compare two AST's structure & content without caring about IDs
+  return false;
+}
+
+
 
 TEST(ParserTest, emptyString) {
   auto b = Parser::parse("");
@@ -19,35 +27,37 @@ TEST(ParserTest, emptyString) {
   EXPECT_EQ(b->countChildren(),0);
 }
 
+TEST(ParserTest, BinaryExp) {
+  auto ast = Parser::parse("a = 5 + 6");
+
+  auto bp = new BinaryExpression(std::make_unique<LiteralInt>(5), Operator(ArithmeticOp::ADDITION), std::make_unique<LiteralInt>(6));
+  auto assignment = Assignment(std::make_unique<Variable>("a"), std::unique_ptr<BinaryExpression>(bp));
+  EXPECT_TRUE(compareAST(*ast, assignment));
+}
+
 TEST(ParserTest, simpleFunction) {
-  auto b = Parser::parse(
-      "public int main() {\n"
-      "  int a = 0;\n"
-      "  a = a + 5;\n"
-      "  return a;\n"
-      "}"
-      );
+  std::string minimal = "public int main() {}";
+  std::string params = "public int main(bool b, float f) {}";
+  std::string body = "public int main() { return 0; }";
 
-  //TODO: Write working parser tests
-//  std::string minimal = "int main() {}";
-//  std::string params = "int main(bool b, float f) {}";
-//  std::string body = "int main() { return 0; }";
-//
-//  auto parsed_minimal = Parser::parse(minimal);
-//  auto parsed_params = Parser::parse(params);
-//  auto parsed_body = Parser::parse(body);
-//
-//  auto expected_minimal = Function(INT, "main", empty_params, std::make_unique<Block>());
-//
-//  auto parameter_vector =
-//      {std::make_unique<FunctionParameter>("b", BOOL), std::make_unique<FunctionParameter>("f", FLOAT)};
-//  auto expected_params = Function(INT, "main", parameter_vector, std::make_unique<Block>());
-//
-//  auto body_node = std::make_unique<Block>(std::make_unique<Return>(std::make_unique<LiteralBool>(0)));
-//  auto expected_body = Function(INT, "main", empty_params, std::move(body_node));
+  auto parsed_minimal = Parser::parse(minimal);
+  auto parsed_params = Parser::parse(params);
+  auto parsed_body = Parser::parse(body);
 
-  //TODO: Write helper function to compare two AST's structure & content without caring about IDs
-  //EXPECT_EQ(*parsed_minimal, expected_minimal);
-  //EXPECT_EQ(*parsed_params, expected_params);
-  //EXPECT_EQ(*parsed_body, expected_body);
+  auto empty_params = std::vector<std::unique_ptr<FunctionParameter>>();
+  auto expected_minimal = Function(INT, "main", std::move(empty_params), std::make_unique<Block>());
+
+  std::vector<std::unique_ptr<FunctionParameter>> parameter_vector;
+  parameter_vector.push_back(std::make_unique<FunctionParameter>(BOOL, "b"));
+  parameter_vector.push_back(std::make_unique<FunctionParameter>(FLOAT, "f"));
+  auto expected_params = Function(INT, "main", std::move(parameter_vector), std::make_unique<Block>());
+
+  empty_params = std::vector<std::unique_ptr<FunctionParameter>>();
+  auto body_node = std::make_unique<Block>(std::make_unique<Return>(std::make_unique<LiteralBool>(0)));
+  auto expected_body = Function(INT, "main", std::move(empty_params), std::move(body_node));
+
+
+  EXPECT_TRUE( compareAST(*parsed_minimal, expected_minimal));
+  EXPECT_TRUE( compareAST(*parsed_params, expected_params));
+  EXPECT_TRUE( compareAST(*parsed_body, expected_body));
 }
