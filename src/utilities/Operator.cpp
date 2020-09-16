c9ca5cbe6e45ed952bfa356e5ac0397b37c87619
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-09-16 10:47:14 +0200

    extend Operator with precedence and associativity notions

--- src/utilities/Operator.cpp
+++ src/utilities/Operator.cpp
@@ -24,8 +24,58 @@ std::string toString(OperatorVariant opVar) {
   }
 }
 
-Operator::Operator(OperatorVariant op)  : op(op) {}
+Operator::Operator(OperatorVariant op) : op(op) {}
 
+bool Operator::isRightAssociative() const {
+  // Only UnaryOp are right associative
+  return op.index()==2;
+}
 std::string Operator::toString() const {
   return ::toString(op);
+}
+
+int comparePrecedence(const Operator &op1, const Operator &op2) {
+
+  // Based on https://en.cppreference.com/w/cpp/language/operator_precedence
+  // Lower number means HIGHER precedence!
+  std::map<OperatorVariant, int> precedence_levels =
+      {
+
+          {UnaryOp::LOGICAL_NOT, 3},
+          {UnaryOp::BITWISE_NOT, 3},
+
+          {ArithmeticOp::MULTIPLICATION, 5},
+          {ArithmeticOp::DIVISION, 5},
+          {ArithmeticOp::MODULO, 5},
+          {ArithmeticOp::ADDITION, 6},
+          {ArithmeticOp::SUBTRACTION, 6},
+
+          {LogicalOp::SMALLER, 9},
+          {LogicalOp::SMALLER_EQUAL, 9},
+          {LogicalOp::GREATER_EQUAL, 9},
+          {LogicalOp::GREATER, 9},
+
+          {LogicalOp::EQUAL, 10},
+          {LogicalOp::UNEQUAL, 10},
+
+          {LogicalOp::BITWISE_AND, 11},
+
+          {LogicalOp::BITWISE_XOR, 12},
+
+          {LogicalOp::BITWISE_OR, 13},
+
+          {LogicalOp::LOGICAL_AND, 14},
+
+          // There is no LOGICAL_XOR since it's the same as UNEQUAL
+
+          {LogicalOp::LOGICAL_OR, 15}
+      };
+
+  if (precedence_levels.at(op1.op) < precedence_levels.at(op2.op)) {
+    return 1;
+  } else if (precedence_levels.at(op1.op)==precedence_levels.at(op2.op)) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
