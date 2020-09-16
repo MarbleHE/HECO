0bcaf95da0c2fec015abaf21c51e878f4f012b03
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-09-16 16:15:01 +0200

    WIP ast comparison

--- test/parser/ParserTest.cpp
+++ test/parser/ParserTest.cpp
@@ -1,11 +1,20 @@
+#include <typeinfo>
+
 #include "ast_opt/ast/Assignment.h"
 #include "ast_opt/ast/BinaryExpression.h"
-#include <include/ast_opt/parser/PushBackStream.h>
+#include "ast_opt/ast/Block.h"
+#include "ast_opt/ast/ExpressionList.h"
+#include "ast_opt/ast/For.h"
 #include "ast_opt/ast/Function.h"
 #include "ast_opt/ast/FunctionParameter.h"
-#include "ast_opt/ast/Literal.h"
-#include "ast_opt/ast/Return.h"
 #include "ast_opt/ast/If.h"
+#include "ast_opt/ast/IndexAccess.h"
+#include "ast_opt/ast/Literal.h"
+#include "ast_opt/ast/OperatorExpression.h"
+#include "ast_opt/ast/Return.h"
+#include "ast_opt/ast/UnaryExpression.h"
+#include "ast_opt/ast/Variable.h"
+#include "ast_opt/ast/VariableDeclaration.h"
 #include "ast_opt/parser/Parser.h"
 #include "gtest/gtest.h"
 #include "ParserTestHelpers.h"
@@ -17,25 +26,87 @@ Datatype FLOAT = Datatype(Type::FLOAT);
 Datatype DOUBLE = Datatype(Type::DOUBLE);
 Datatype STRING = Datatype(Type::STRING);
 
-bool compareAST(const AbstractNode &ast1, const AbstractNode &ast2) {
-  //TODO: Write helper function to compare two AST's structure & content without caring about IDs
-  return false;
+bool compareAST(const AbstractNode& ast1, const AbstractNode& ast2) {
+  //TODO: Don't just return bool, instead provide google test failures
+  bool same_attrib = true;
+  if (typeid(ast1) != typeid(ast2)) {
+    return false;
+  } else  if(typeid(ast1) == typeid(const Assignment&)) {
+    // No non-AST attributes
+  } else if(typeid(ast1) == typeid(const BinaryExpression&)) {
+   auto b1 = dynamic_cast<const BinaryExpression&>(ast1);
+   auto b2 = dynamic_cast<const BinaryExpression&>(ast2);
+   same_attrib = b1.getOperator().toString() == b2.getOperator().toString();
+  } else if(typeid(ast1) == typeid(Block)) {
+    // No non-AST attributes
+  }else if(typeid(ast1) == typeid(ExpressionList)) {
+    // No non-AST attributes
+  }else if(typeid(ast1) == typeid(For)) {
+    // No non-AST attributes
+  }else if(typeid(ast1) == typeid(const Function&)) {
+    auto f1 = dynamic_cast<const Function&>(ast1);
+    auto f2 = dynamic_cast<const Function&>(ast2);
+    same_attrib = f1.getIdentifier() == f2.getIdentifier()
+        && f1.getReturnType() == f2.getReturnType();
+  }else if(typeid(ast1) == typeid(const FunctionParameter&)) {
+    auto f1 = dynamic_cast<const FunctionParameter&>(ast1);
+    auto f2 = dynamic_cast<const FunctionParameter&>(ast2);
+    same_attrib = f1.getIdentifier() == f2.getIdentifier()
+        && f1.getParameterType() == f2.getParameterType();
+  }else if(typeid(ast1) == typeid(const If&)) {
+    // No non-AST attributes
+  }else if(typeid(ast1) == typeid(IndexAccess)) {
+  // No non-AST attributes
+  }else if(typeid(ast1) == typeid(const OperatorExpression&)) {
+    auto o1 = dynamic_cast<const OperatorExpression&>(ast1);
+    auto o2 = dynamic_cast<const OperatorExpression&>(ast2);
+    same_attrib = o1.getOperator().toString() == o2.getOperator().toString();
+  } else if(typeid(ast1) == typeid(Return)) {
+    // No non-AST attributes
+  }
+  else if(typeid(ast1) == typeid(const UnaryExpression&)) {
+    auto u1 = dynamic_cast<const UnaryExpression&>(ast1);
+    auto u2 = dynamic_cast<const UnaryExpression&>(ast2);
+    same_attrib = u1.getOperator().toString() == u2.getOperator().toString();
+  }else if(typeid(ast1) == typeid(const Variable&)) {
+    auto v1 = dynamic_cast<const Variable&>(ast1);
+    auto v2 = dynamic_cast<const Variable&>(ast2);
+    same_attrib = v1.getIdentifier() == v2.getIdentifier();
+  }else if(typeid(ast1) == typeid(const VariableDeclaration&)) {
+    auto v1 = dynamic_cast<const VariableDeclaration&>(ast1);
+    auto v2 = dynamic_cast<const VariableDeclaration&>(ast2);
+    same_attrib = v1.getDatatype() == v2.getDatatype();
+  } else {
+    //TODO: Check if they're literals!!
+    throw std::runtime_error("Something bad happened while comparing ASTs.");
+  }
+
+  // Compare Children
+  if(ast1.countChildren() != ast2.countChildren())
+    return false;
+  bool children_same = true;
+  auto it1 = ast1.begin();
+  auto it2 = ast2.begin();
+  for(;it1 != ast1.end() && it2 != ast2.end(); ++it1, ++it2)   {
+    children_same = children_same && compareAST(*it1,*it2);
+  }
+  return same_attrib && children_same;
 }
+
+
 
 TEST(ParserTest, emptyString) {
   auto b = Parser::parse("");
   // Should be an empty block
-  EXPECT_EQ(b->countChildren(), 0);
+  EXPECT_EQ(b->countChildren(),0);
 }
 
 TEST(ParserTest, BinaryExp) {
-  auto ast = Parser::parse("a = 5 + 6");
+  auto ast = Parser::parse("a = 5 + 6;");
 
-  auto bp = new BinaryExpression(std::make_unique<LiteralInt>(5),
-                                 Operator(ArithmeticOp::ADDITION),
-                                 std::make_unique<LiteralInt>(6));
+  auto bp = new BinaryExpression(std::make_unique<LiteralInt>(5), Operator(ArithmeticOp::ADDITION), std::make_unique<LiteralInt>(6));
   auto assignment = Assignment(std::make_unique<Variable>("a"), std::unique_ptr<BinaryExpression>(bp));
-  EXPECT_TRUE(compareAST(*ast, assignment));
+  EXPECT_TRUE(compareAST(*ast->begin(), assignment));
 }
 
 TEST(ParserTest, simpleFunction) {
