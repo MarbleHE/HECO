a6d1dbb98a34963f7cd7d301d1bc2617046bd25a
Patrick Jattke <pjattke@ethz.ch>
2020-09-17 10:54:35 +0200

    Adds ignore to HidingNonVirtualFunction clang error
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/ast/AbstractTarget.h
+++ include/ast_opt/ast/AbstractTarget.h
@@ -6,13 +6,16 @@
 /// It represents "things that can be assigned to", i.e. lvalues
 class AbstractTarget : public AbstractExpression {
  public:
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "HidingNonVirtualFunction"
   /// Clones a node recursively, i.e., by including all of its children.
   /// Because return-type covariance does not work with smart pointers,
   /// derived classes are expected to introduce a std::unique_ptr<DerivedNode> clone() method that hides this (for use with derived class ptrs/refs)
   /// \return A clone of the node including clones of all of its children.
-  inline std::unique_ptr<AbstractTarget> clone() const { /* NOLINT intentionally hiding */
+  inline std::unique_ptr<AbstractTarget> clone() const { /* intentionally hiding */
     return std::unique_ptr<AbstractTarget>(clone_impl());
   }
+#pragma clang diagnostic pop
  private:
   /// Refines return type to AbstractTarget
   AbstractTarget *clone_impl() const override = 0;
