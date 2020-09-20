b8d9e770b428f90e48c458599d14203af7340b27
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-09-20 12:41:18 +0200

    additional test cases + todos for (not yet implemented) Vectorizer

--- test/visitor/VectorizerTest.cpp
+++ test/visitor/VectorizerTest.cpp
@@ -53,8 +53,81 @@ TEST(VectorizerTest, singleOutlierVector) {
 
   const char *expectedChars = R""""(
     x = y;
-    x *= {1,1,1,1,1,1,1,1,0};
-    x += {0,0,0,0,0,0,0,0,5};
+    x = x * {1,1,1,1,1,1,1,1,0};
+    x = x + {0,0,0,0,0,0,0,0,5};
+    )"""";
+  auto expectedCode = std::string(expectedChars);
+  auto expectedAST = Parser::parse(expectedCode);
+
+  EXPECT_TRUE(compareAST(*inputAST,*expectedAST));
+}
+
+
+TEST(VectorizerTest, sumStatementsPowerOfTwo) {
+  //If sum is vector valued, this would mean something very different
+  // Specifically, it would mean that in each step, x[i] is added to each slot.
+  // TODO: Therefore, we need to pass in some additional information to the Vectorizer
+  // i.e. that sum has a scalar datatype.
+  //TODO: We *do* need to differentiate between vector and scalar types in our input language and during compilation!
+  const char *inputChars = R""""(
+    sum = sum + x[0];
+    sum = sum + x[1];
+    sum = sum + x[2];
+    sum = sum + x[3];
+    sum = sum + x[4];
+    sum = sum + x[5];
+    sum = sum + x[6];
+    sum = sum + x[7];
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  Vectorizer v;
+  inputAST->accept(v);
+
+  //TODO: How to communicate the slot of the result to the runtime system?
+  // maybe something like __sum__i__ where i is the slot index?
+  // Then we could get rid of the sum = sum * {1,0,0...} masking + it would be less brittle
+  const char *expectedChars = R""""(
+    sum = x + rotate(x, 4);
+    sum = sum + rotate(sum, 2);
+    sum = sum + rotate(sum, 1);
+    sum = sum * {1,0,0,0,0,0,0,0};
+    )"""";
+  auto expectedCode = std::string(expectedChars);
+  auto expectedAST = Parser::parse(expectedCode);
+
+  EXPECT_TRUE(compareAST(*inputAST,*expectedAST));
+}
+
+TEST(VectorizerTest, sumStatementsGeneral) {
+  const char *inputChars = R""""(
+    sum = sum + x[0];
+    sum = sum + x[1];
+    sum = sum + x[2];
+    sum = sum + x[3];
+    sum = sum + x[4];
+    sum = sum + x[5];
+    sum = sum + x[6];
+    sum = sum + x[7];
+    sum = sum + x[8];
+    sum = sum + x[9];
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  Vectorizer v;
+  inputAST->accept(v);
+
+  // First extend the vector to the next power of two (and mask away any potential garbage)
+  // TODO: Is there a away to avoid this by keeping some additional info or having invariants/guarantees on inputs?
+  const char *expectedChars = R""""(
+    sum = x * {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0};
+    sum = sum + rotate(sum, 8);
+    sum = sum + rotate(sum, 4);
+    sum = sum + rotate(sum, 2);
+    sum = sum + rotate(sum, 1);
+    sum = sum * {1,0,0,0,0,0,0,0};
     )"""";
   auto expectedCode = std::string(expectedChars);
   auto expectedAST = Parser::parse(expectedCode);
