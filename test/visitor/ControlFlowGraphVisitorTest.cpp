218691e58b12cd9cc1ed44a9b964491302ecd541
Patrick Jattke <pjattke@ethz.ch>
2020-09-22 14:48:41 +0200

    Adds more tests for the control flow graph
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- test/visitor/ControlFlowGraphVisitorTest.cpp
+++ test/visitor/ControlFlowGraphVisitorTest.cpp
@@ -11,8 +11,6 @@
 
 // == Tests for the Control Flow Graph
 
-// TODO: Add some tests to see whether CFG is properly constructed.
-
 std::vector<std::reference_wrapper<GraphNode>> createParentsVector(std::initializer_list<GraphNode *> nodes) {
   std::vector<std::reference_wrapper<GraphNode>> parents;
   for (auto node : nodes) parents.push_back(std::ref(*node));
@@ -41,13 +39,101 @@ TEST(ControlFlowGraphVisitorTest, cfg_simpleProgram) { /* NOLINT */
   auto varDeclStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({blockStmt.get()}));
   auto returnStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varDeclStmt.get()}));
 
-// check that the CFG's structure is correct
+  // check that the CFG's structure is correct
   EXPECT_TRUE(functionStmt->getControlFlowGraph().isEqualToGraph(cfgv.getRootNode()));
 
-// check that all nodes of the CFG have a refToOriginalNode set
+  // check that all nodes of the CFG have a refToOriginalNode set
   auto allNodes = cfgv.getRootNode().getControlFlowGraph().getAllReachableNodes();
+  // Note that we added 1 because the Parser always wraps the parsed statements into a Block
   EXPECT_EQ(allNodes.size(), 5);
 }
+
+TEST(ControlFlowGraphVisitorTest, cfg_ifElseProgram) { /* NOLINT */
+  const char *inputChars = R""""(
+    public int main(int a) {
+      int q;
+      q = 21;
+      if (q > a) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  auto &gn = cfgv.getRootNode();
+
+  const auto relType = RelationshipType::CTRL_FLOW_GRAPH;
+  auto dummyAstNode = Block();
+  auto functionStmt = std::make_unique<GraphNode>(dummyAstNode);
+  auto blockStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({functionStmt.get()}));
+  auto varDeclStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({blockStmt.get()}));
+  auto varAssignmStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varDeclStmt.get()}));
+  auto ifAssignmStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varAssignmStmt.get()}));
+  auto thenBranch = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({ifAssignmStmt.get()}));
+  auto thenReturnStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({thenBranch.get()}));
+  auto elseBranch = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({ifAssignmStmt.get()}));
+  auto elseReturnStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({elseBranch.get()}));
+
+  // check that the CFG's structure is correct
+  EXPECT_TRUE(functionStmt->getControlFlowGraph().isEqualToGraph(cfgv.getRootNode()));
+
+  // check that all nodes of the CFG have a refToOriginalNode set
+  auto allNodes = cfgv.getRootNode().getControlFlowGraph().getAllReachableNodes();
+  // Note that we added 1 because the Parser always wraps the parsed statements into a Block
+  EXPECT_EQ(allNodes.size(), 10);
+}
+
+TEST(ControlFlowGraphVisitorTest, cfg_ForProgram) { /* NOLINT */
+  const char *inputChars = R""""(
+    public int main(int a) {
+      int q;
+      q = 21;
+      for (int i = 0; i < 22; i = i+1) {
+        q = q + i * 12;
+      }
+      return q;
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  auto &gn = cfgv.getRootNode();
+
+  const auto relType = RelationshipType::CTRL_FLOW_GRAPH;
+  auto dummyAstNode = Block();
+  auto functionStmt = std::make_unique<GraphNode>(dummyAstNode);
+  auto blockStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({functionStmt.get()}));
+  auto varDeclStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({blockStmt.get()}));
+  auto varAssignmStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varDeclStmt.get()}));
+  auto forStatement = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varAssignmStmt.get()}));
+  auto forInitializer = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({forStatement.get()}));
+
+  auto forBlock = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({forInitializer.get()}));
+  auto varAssignmStmt2 = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({forBlock.get()}));
+  auto forUpdate = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({varAssignmStmt2.get()}));
+  varAssignmStmt2->getControlFlowGraph().addChild(*forUpdate);
+
+  auto returnStmt = std::make_unique<GraphNode>(dummyAstNode, relType, createParentsVector({forUpdate.get()}));
+
+  // check that the CFG's structure is correct
+  EXPECT_TRUE(functionStmt->getControlFlowGraph().isEqualToGraph(cfgv.getRootNode()));
+
+  // check that all nodes of the CFG have a refToOriginalNode set
+  auto allNodes = cfgv.getRootNode().getControlFlowGraph().getAllReachableNodes();
+  // Note that we added 1 because the Parser always wraps the parsed statements into a Block.
+  // Also, note that initializer and update of the For loop are wrapped into blocks each.
+  EXPECT_EQ(allNodes.size(), 13);
+}
+
 
 // == Tests for the Data Flow Graph
 
