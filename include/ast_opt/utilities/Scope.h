843da2c301d7440c87d9413bf1639eeb2ef4dbf6
Patrick Jattke <pjattke@ethz.ch>
2020-09-22 19:01:02 +0200

    Implements and documents major part of Scope logic
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/utilities/Scope.h
+++ include/ast_opt/utilities/Scope.h
@@ -1,5 +1,6 @@
 #ifndef AST_OPTIMIZER_INCLUDE_AST_OPT_UTILITIES_SCOPE_H_
 #define AST_OPTIMIZER_INCLUDE_AST_OPT_UTILITIES_SCOPE_H_
+
 #include <tuple>
 #include <set>
 #include "ast_opt/ast/AbstractNode.h"
@@ -8,10 +9,10 @@
 
 class ScopedIdentifier;
 
-class Scope{
+class Scope {
  private:
-  /// (weak) pointer to the AST node that creates this scope
-  AbstractNode *node = nullptr;
+  /// (Weak) pointer to the AST node that creates this scope
+  AbstractNode &astNode;
 
   /// Set of identifiers declared in this scope
   std::set<std::unique_ptr<ScopedIdentifier>> identifiers;
@@ -19,47 +20,89 @@ class Scope{
   /// Parent scope (if it exists)
   Scope *parent = nullptr;
 
+  /// The scopes that are nested in this scope
+  std::vector<std::unique_ptr<Scope>> nestedScopes;
+
  public:
+  /// Creates a new scope.
+  /// \param abstractNode The node that creates this scope.
+  explicit Scope(AbstractNode &abstractNode);
 
-  Scope();
-
+  /// Adds an declared identifier (e.g., variable) to this scope.
+  /// \param id The identifier to be added to this scope.
   void addIdentifier(const std::string &id);
 
-  bool identifierIsLocal(const std::string &id) const;
+  /// Checks whether the given identifier is local, i.e., declared in this scope and not in any parent scope.
+  /// \param id The identifier to be checked.
+  /// \return True iff the given identifier id is defined in this scope.
+  [[nodiscard]] bool identifierIsLocal(const std::string &id) const;
 
-  bool identifierExists(const std::string &id) const;
+  /// Checks whether the given identifier id exists in this or any parent scope.
+  /// \param id The identifier to be checked.
+  /// \return True iff the given identifier is defined in this or any parent scope.
+  [[nodiscard]] bool identifierExists(const std::string &id) const;
 
+  /// Sets the parent scope of this scope.
+  /// \param parentScope The scope to be set as parent of this scope.
+  void setParent(Scope *parentScope);
+
+  /// Creates a nested scope within this scope.
+  /// \param scope The scope to add as nested scope in this scope.
+  void addNestedScope(std::unique_ptr<Scope> &&scope);
+
+  /// Gets the parent scope of this scope.
+  /// \return (A reference to) the parent scope of this scope.
   Scope &getParentScope();
 
-  const Scope &getParentScope() const;
+  /// Gets the parent scope of this scope.
+  /// \return (A const reference to) the parent scope of this scope.
+  [[nodiscard]] const Scope &getParentScope() const;
 
+  /// Creates a nested scope with the given parentScope as parent and the given AbstractNode scopeOpener as the AST node
+  /// that creates this scope.
+  /// \param parentScope The parent scope of the scope to be created.
+  /// \param scopeOpener The AST node that creates the nested scope.
+  /// \return (A weak pointer) to the nested scope that is created.
+  static Scope *createNestedScope(Scope &parentScope, AbstractNode &scopeOpener);
+
+  /// Determines the ScopedIdentifier of the given identifier.
+  /// \param id The identifier for that the ScopedIdentifier should be determined.
+  /// \return (A const reference) to the ScopedIdentifier object associated with the given identifier.
   [[nodiscard]] const ScopedIdentifier &resolveIdentifier(const std::string &id) const;
 };
 
 class ScopedIdentifier {
  private:
   /// (weak) pointer to the Scope this identifier belongs to
-  Scope *scope;
+  Scope &scope;
 
-  /// identifier
+  /// identifier (e.g., variable's name)
   std::string id;
 
  public:
+  /// Creates a new ScopedIdentifier.
+  /// \param scope The scope where the identifier was declared in.
+  /// \param id The identifier that is defined in the given scope.
+  ScopedIdentifier(Scope &scope, std::string id);
+
   bool operator<(const ScopedIdentifier &fk) const {
     return id < fk.id;
   }
 
-  Scope *getScope();
+  /// Gets the scope associated with this ScopedIdentifier.
+  /// \return (A reference to) the scope of this ScopedIdentifier.
+  Scope &getScope();
 
-  const Scope *getScope() const;
+  /// Gets the scope associated with this ScopedIdentifier.
+  /// \return (A const reference to) the scope of this ScopedIdentifier.
+  [[nodiscard]] const Scope &getScope() const;
 
-  void setScope(Scope *scope);
+  /// Gets the identifier associated with this ScopedIdentifier.
+  /// \return (A const string reference to) the identifier of this ScopedIdentifier.
+  [[nodiscard]] const std::string &getId() const;
 
-  const std::string &getId() const;
-
+  /// Gets the identifier associated with this ScopedIdentifier.
+  /// \return (A string reference to) the identifier of this ScopedIdentifier.
   std::string &getId();
-
-  void setId(const std::string &id);
-
 };
 #endif //AST_OPTIMIZER_INCLUDE_AST_OPT_UTILITIES_SCOPE_H_
--- src/utilities/Scope.cpp
+++ src/utilities/Scope.cpp
@@ -1,24 +1,66 @@
+#include <utility>
+
 #include "ast_opt/utilities/Scope.h"
 
 const ScopedIdentifier &Scope::resolveIdentifier(const std::string &id) const {
-  // TODO: Replace dummy function body
-  return **identifiers.begin();
+  // TODO Implement and throw std::runtime_error in case that identifier cannot be resolved
+  throw std::runtime_error("Scope::resolveIdentifier unimplemented!");
 }
 
-Scope::Scope() {
-  // TODO: Replace dummy function body
-  identifiers.emplace(std::make_unique<ScopedIdentifier>());
+void Scope::addIdentifier(const std::string &id) {
+  identifiers.insert(std::make_unique<ScopedIdentifier>(*this, id));
 }
-Scope *ScopedIdentifier::getScope() {
+
+Scope &Scope::getParentScope() {
+  return *parent;
+}
+
+const Scope &Scope::getParentScope() const {
+  return *parent;
+}
+
+Scope *Scope::createNestedScope(Scope &parentScope, AbstractNode &scopeOpener) {
+  auto scope = std::make_unique<Scope>(scopeOpener);
+  Scope *scopePtr = scope.get();
+  scope->setParent(&parentScope);
+  scope->addNestedScope(std::move(scope));
+  return scopePtr;
+}
+
+void Scope::setParent(Scope *parentScope) {
+  Scope::parent = parentScope;
+}
+
+void Scope::addNestedScope(std::unique_ptr<Scope> &&scope) {
+  nestedScopes.push_back(std::move(scope));
+}
+
+Scope::Scope(AbstractNode &abstractNode) : astNode(abstractNode) {
+
+}
+
+bool Scope::identifierExists(const std::string &id) const {
+  try {
+    auto scopedIdentifier = resolveIdentifier(id);
+  } catch (std::runtime_error &) {
+    return false;
+  }
+  return true;
+}
+
+bool Scope::identifierIsLocal(const std::string &id) const {
+  // go through all identifiers that are declared in this scope and check whether the identifier matches the given one
+  return std::any_of(identifiers.begin(), identifiers.end(), [id](const auto &scopedIdentifier) {
+    return scopedIdentifier->getId()==id;
+  });
+}
+
+Scope &ScopedIdentifier::getScope() {
   return scope;
 }
 
-const Scope *ScopedIdentifier::getScope() const {
+const Scope &ScopedIdentifier::getScope() const {
   return scope;
-}
-
-void ScopedIdentifier::setScope(Scope *scope) {
-  ScopedIdentifier::scope = scope;
 }
 
 const std::string &ScopedIdentifier::getId() const {
@@ -29,6 +71,4 @@ std::string &ScopedIdentifier::getId() {
   return id;
 }
 
-void ScopedIdentifier::setId(const std::string &id) {
-  ScopedIdentifier::id = id;
-}
+ScopedIdentifier::ScopedIdentifier(Scope &scope, std::string id) : scope(scope), id(std::move(id)) {}
