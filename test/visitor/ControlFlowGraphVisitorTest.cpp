3738d2670a379b7b7c6c4373e03a247f02c9aa1c
Patrick Jattke <pjattke@ethz.ch>
2020-09-22 08:31:37 +0200

    WIP CFGV: Adds more tests
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- test/visitor/ControlFlowGraphVisitorTest.cpp
+++ test/visitor/ControlFlowGraphVisitorTest.cpp
@@ -9,8 +9,31 @@
 #include "include/ast_opt/visitor/ControlFlowGraph/ControlFlowGraphVisitor.h"
 #include "gtest/gtest.h"
 
+// == Tests for the Control Flow Graph
+
+// TODO: Add some tests to see whether CFG is properly constructed.
+
+TEST(ControlFlowGraphVisitorTest, forLoop_localVariable_emptyUpdate) { /* NOLINT */
+  const char *inputChars = R""""(
+    int main() {
+      int a = 10;
+      return a;
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  // TODO: Complete test after having specified public interface
+  std::cout << "END." << std::endl;
+}
+
+// == Tests for the Data Flow Graph
+
 bool setContains(const std::set<VariableAccessPair> &variableAccesses,
-                 std::string expectedIdentifier,
+                 const std::string &expectedIdentifier,
                  VariableAccessType expectedAccessType) {
   for (auto[scopeIdentifier, accessType] : variableAccesses) {
     if (scopeIdentifier.getId()==expectedIdentifier
@@ -22,7 +45,7 @@ bool setContains(const std::set<VariableAccessPair> &variableAccesses,
 }
 
 GraphNode &getNthStatementInBlock(ControlFlowGraphVisitor &cfgv, int n) {
-  return cfgv.getRootNode().getControlFlowGraph().getChildAtIndex(n);
+  return cfgv.getRootNode().getDataFlowGraph().getChildAtIndex(n);
 }
 
 TEST(ControlFlowGraphVisitorTest, noScopeGiven_expectFail) { /* NOLINT */
@@ -48,7 +71,7 @@ TEST(ControlFlowGraphVisitorTest, simpleAssignment) { /* NOLINT */
   ControlFlowGraphVisitor cfgv;
   inputAST->accept(cfgv);
 
-  // get first child as Block is the root node
+  // get first child as Block (is the root node)
   auto accessedVariables = getNthStatementInBlock(cfgv, 0).getAccessedVariables();
 
   EXPECT_EQ(accessedVariables.size(), 1);
@@ -88,11 +111,117 @@ TEST(ControlFlowGraphVisitorTest, ifStatement) { /* NOLINT */
   ControlFlowGraphVisitor cfgv;
   inputAST->accept(cfgv);
 
-  auto accessedVariables = getNthStatementInBlock(cfgv, 0).getAccessedVariables();
+  auto accessedVariables_ifStatement = getNthStatementInBlock(cfgv, 0).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_ifStatement.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_ifStatement, "c", VariableAccessType::READ));
 
-  EXPECT_EQ(accessedVariables.size(), 2);
-  EXPECT_TRUE(setContains(accessedVariables, "q", VariableAccessType::WRITE));
-  EXPECT_TRUE(setContains(accessedVariables, "a", VariableAccessType::READ));
+  auto accessedVariables_thenBlock =
+      getNthStatementInBlock(cfgv, 0).getDataFlowGraph().getChildAtIndex(1).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_thenBlock.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_thenBlock, "a", VariableAccessType::WRITE));
+}
+
+TEST(ControlFlowGraphVisitorTest, ifElseStatement) { /* NOLINT */
+  const char *inputChars = R""""(
+    {
+      if (c > 100) {
+        a = 22;
+      } else {
+        a = 43;
+      }
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  auto accessedVariables_ifStatement = getNthStatementInBlock(cfgv, 0).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_ifStatement.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_ifStatement, "c", VariableAccessType::READ));
+
+  auto accessedVariables_thenBlock =
+      getNthStatementInBlock(cfgv, 0).getDataFlowGraph().getChildAtIndex(1).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_thenBlock.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_thenBlock, "a", VariableAccessType::WRITE));
+
+  auto accessedVariables_elseBlock =
+      getNthStatementInBlock(cfgv, 0).getDataFlowGraph().getChildAtIndex(2).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_thenBlock.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_thenBlock, "a", VariableAccessType::WRITE));
+}
+
+TEST(ControlFlowGraphVisitorTest, forLoop_accumulation) { /* NOLINT */
+  const char *inputChars = R""""(
+    {
+      int sum = 0;
+      for (int i = 0; i < 100; ++i) {
+        sum = sum + 1;
+      }
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  auto accessedVariables_initializer =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(0).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::WRITE));
+
+  auto accessedVariables_condition =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(1).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::READ));
+
+  auto accessedVariables_update =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(2).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::WRITE));
+
+  auto accessedVariables_block =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(3).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 2);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "sum", VariableAccessType::READ));
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "sum", VariableAccessType::WRITE));
+}
+
+TEST(ControlFlowGraphVisitorTest, forLoop_localVariable_emptyUpdate) { /* NOLINT */
+  const char *inputChars = R""""(
+    {
+      for (int i = 0; i < 100; ) {
+        int c = i+1;
+      }
+    }
+    )"""";
+  auto inputCode = std::string(inputChars);
+  auto inputAST = Parser::parse(inputCode);
+
+  ControlFlowGraphVisitor cfgv;
+  inputAST->accept(cfgv);
+
+  auto accessedVariables_initializer =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(0).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::WRITE));
+
+  auto accessedVariables_condition =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(1).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 1);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::READ));
+
+  auto accessedVariables_update =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(2).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 0);
+
+  auto accessedVariables_block =
+      getNthStatementInBlock(cfgv, 1).getDataFlowGraph().getChildAtIndex(3).getAccessedVariables();
+  EXPECT_EQ(accessedVariables_initializer.size(), 2);
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "i", VariableAccessType::READ));
+  EXPECT_TRUE(setContains(accessedVariables_initializer, "c", VariableAccessType::WRITE));
 }
 
 // TODO: add many more test programs
