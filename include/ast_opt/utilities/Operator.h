13fbfe1f8cd72f35bfbff34c6af24a01ea06a733
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-09-29 22:26:19 +0200

    add operator== for Operators

--- include/ast_opt/utilities/Operator.h
+++ include/ast_opt/utilities/Operator.h
@@ -53,7 +53,14 @@ class Operator {
   [[nodiscard]] bool isUnary() const;
 
   [[nodiscard]] std::string toString() const;
+
+  friend bool operator==(const Operator& op1, const Operator& op2);
 };
+
+
+/// Compares two Operators for equality
+inline bool operator==(const Operator& op1, const Operator& op2) { return op1.op == op2.op;}
+
 
 /// Compares the precedence of this operator against another operator.
 /// \param op1 First operator
