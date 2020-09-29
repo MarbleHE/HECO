28768b184ac2103bc1e2f79076ee9858bdb28291
Patrick Jattke <pjattke@ethz.ch>
2020-09-29 21:05:02 +0200

    WIP SecretBranchingVisitor
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/SecretBranchingVisitor.h
+++ include/ast_opt/visitor/SecretBranchingVisitor.h
@@ -1,7 +1,9 @@
 #ifndef GRAPHNODE_H_SRC_VISITOR_SECRETBRANCHINGVISITOR_H_
 #define GRAPHNODE_H_SRC_VISITOR_SECRETBRANCHINGVISITOR_H_
 
+#include "ast_opt/ast/AbstractExpression.h"
 #include "ast_opt/visitor/ScopedVisitor.h"
+#include "TypeCheckingVisitor.h"
 
 // Forward declaration
 class SpecialSecretBranchingVisitor;
@@ -9,16 +11,36 @@ class SpecialSecretBranchingVisitor;
 /// ControlFlowGraphVisitor uses the Visitor<T> template to allow specifying default behaviour
 typedef Visitor<SpecialSecretBranchingVisitor> SecretBranchingVisitor;
 
+/// value must be a ptr as variable can be initialized but without a value
+typedef std::unordered_map<ScopedIdentifier, AbstractExpression *> VariableValueMap;
+
 class SpecialSecretBranchingVisitor : public ScopedVisitor {
  private:
   bool unsupportedBodyStatementVisited = false;
 
+  VariableValueMap expressionValues;
+
+  SecretTaintedNodesMap &secretTaintedNodesMap;
+
  public:
+
+  explicit SpecialSecretBranchingVisitor(SecretTaintedNodesMap &taintedNodesMap);
+
   void visit(If &node) override;
 
   void visit(For &elem) override;
 
   void visit(Return &elem) override;
+
+  void visit(Assignment &elem) override;
+
+  static VariableValueMap getChangedVariables(const VariableValueMap &baseMap, const VariableValueMap &changedMap);
+
+  void addIdentifiers(Scope &scope);
+
+  void visit(VariableDeclaration &node) override;
+
+  void visit(FunctionParameter &node) override;
 };
 
 #endif //GRAPHNODE_H_SRC_VISITOR_SECRETBRANCHINGVISITOR_H_
--- src/visitor/SecretBranchingVisitor.cpp
+++ src/visitor/SecretBranchingVisitor.cpp
@@ -1,13 +1,89 @@
+#include "ast_opt/ast/Variable.h"
+#include "ast_opt/visitor/TypeCheckingVisitor.h"
 #include "ast_opt/visitor/SecretBranchingVisitor.h"
+#include "ast_opt/ast/FunctionParameter.h"
+#include "ast_opt/ast/VariableDeclaration.h"
+#include "ast_opt/ast/Assignment.h"
 #include "ast_opt/ast/If.h"
+#include <iostream>
 
-void SpecialSecretBranchingVisitor::visit(If &) {
+void SpecialSecretBranchingVisitor::addIdentifiers(Scope &scope) {
+  std::for_each(expressionValues.begin(),
+                expressionValues.end(),
+                [&](std::pair<ScopedIdentifier, AbstractExpression *> key) {
+                  scope.addIdentifier(key.first.getId());
+                });
+}
+
+VariableValueMap SpecialSecretBranchingVisitor::getChangedVariables(const VariableValueMap &baseMap,
+                                                                    const VariableValueMap &changedMap) {
+  VariableValueMap result;
+
+  for (auto &[k, v] : changedMap) {
+    if ((baseMap.count(k)==0)     // a newly declared variable
+        || (v!=changedMap.at(k))) { // a variable whose value changed
+      result.insert_or_assign(k, v);
+    }
+  }
+
+  return result;
+}
+
+void SpecialSecretBranchingVisitor::visit(If &node) {
   unsupportedBodyStatementVisited = false;
 
-  // visit the if statement's condition and body
+  VariableValueMap exprValuesBefore = expressionValues;
+  VariableValueMap exprValuesAfterThen;
+  VariableValueMap exprValuesAfterElse;
 
-  if (!unsupportedBodyStatementVisited) {
-    // do rewriting
+  // visit the if statement's condition
+  node.getCondition().accept(*this);
+
+  // visit the then branch
+  node.getThenBranch().accept(*this);
+  exprValuesAfterThen = expressionValues;
+
+  // if existing, visit the else branch
+  if (node.hasElseBranch()) {
+    expressionValues = exprValuesBefore;
+    node.getElseBranch().accept(*this);
+    exprValuesAfterElse = expressionValues;
+  }
+
+  // check if the condition involves secret value
+  auto conditionNodeId = node.getCondition().getUniqueNodeId();
+  if (secretTaintedNodesMap.count(conditionNodeId)==0) {
+    throw std::runtime_error(
+        "Cannot find secret tainting information for condition node (" + conditionNodeId + ").");
+  }
+  auto isSecretCondition = secretTaintedNodesMap.at(conditionNodeId);
+
+  if (isSecretCondition && !unsupportedBodyStatementVisited) {
+    std::cout << "doing rewriting... " << std::endl;
+//    if (!node.hasElseBranch()) {
+//      for (auto &[k, v] :)
+//      // rewrite to (c)*thenValue+(1-c)*oldValue
+//
+//
+//    } else {
+//      // rewrite to (c)*thenValue+(1-c)*elseValue
+//    }
+//
+//
+//    // get variables changed in both branches
+//    if (sameChangedVariables(expressionValuesAfterThenBranch, expressionValuesAfterElseBranch)) {
+//      // create statements
+//    } else {
+//
+//    }
+
+    // get variables changed in else branch only
+
+    // get variables changed in both then and else branch
+
+
+    // replace existing statement by new one
+
   }
 }
 
@@ -21,3 +97,32 @@ void SpecialSecretBranchingVisitor::visit(Return &node) {
   ScopedVisitor::visit(node);
 }
 
+void SpecialSecretBranchingVisitor::visit(Assignment &node) {
+  if (auto lhsVariable = dynamic_cast<Variable *>(&node.getTarget())) {
+    auto scopedIdentifier = getCurrentScope().resolveIdentifier(lhsVariable->getIdentifier());
+    expressionValues.insert_or_assign(scopedIdentifier, &node.getValue());
+  } else {
+    throw std::runtime_error("SecretBranchingVisitor can handle assignments to variables yet only.");
+  }
+}
+
+void SpecialSecretBranchingVisitor::visit(VariableDeclaration &node) {
+  AbstractExpression *value = nullptr;
+  if (node.hasValue()) {
+    value = &node.getValue();
+  }
+  expressionValues.insert_or_assign(
+      ScopedIdentifier(getCurrentScope(), node.getTarget().getIdentifier()), value);
+  ScopedVisitor::visit(node);
+}
+
+void SpecialSecretBranchingVisitor::visit(FunctionParameter &node) {
+//  auto scopedIdentifier = std::make_unique<ScopedIdentifier>(getCurrentScope(), node.getIdentifier());
+  expressionValues.insert_or_assign(ScopedIdentifier(getCurrentScope(), node.getIdentifier()), nullptr);
+  ScopedVisitor::visit(node);
+}
+
+SpecialSecretBranchingVisitor::SpecialSecretBranchingVisitor(SecretTaintedNodesMap &taintedNodesMap)
+    : secretTaintedNodesMap(taintedNodesMap) {
+
+}
