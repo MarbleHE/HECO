e385132e620ce598e9081b719e381249c8da3d99
Patrick Jattke <pjattke@ethz.ch>
2020-10-01 12:33:22 +0200

    Adds header comments for SecretBranchingVisitor
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/SecretBranchingVisitor.h
+++ include/ast_opt/visitor/SecretBranchingVisitor.h
@@ -16,19 +16,36 @@ typedef std::unordered_map<ScopedIdentifier, AbstractExpression *> VariableValue
 
 class SpecialSecretBranchingVisitor : public ScopedVisitor {
  private:
-  bool unsupportedBodyStatementVisited = false;
-
+  /// A map that stores the current value, while traversing the AST, for each scoped identifier.
   VariableValueMap expressionValues;
 
+  /// The map (passed in constructor) that provides information about expression nodes involving secret variables.
   SecretTaintedNodesMap &secretTaintedNodesMap;
 
+  /// A flag that indicates whether the body of an If statement included an unsupported statement (e.g., For, Return)
+  /// that inhibits rewriting this If statement.
+  bool unsupportedBodyStatementVisited = false;
+
+  /// A flag that helps to propagate the information that an If statement can be deleted to its parent node
+  /// (i.e., a Block statement), which then executes the deletion.
   bool visitedStatementMarkedForDeletion = false;
 
+  /// The statements to be placed at the position where the node marked for deletion was before
+  /// (see visitedStatementMarkedForDeletion).
   std::vector<std::unique_ptr<AbstractStatement>> replacementStatements;
 
  public:
-
+  /// Creates a new SpecialSecretBranchingVisitor by taking a map of <unique node ID, bool> as input that provides
+  /// information about expressions dealing with secret variables.
+  /// \param taintedNodesMap A map of <unique node ID, bool> containing information about secret tainted nodes.
   explicit SpecialSecretBranchingVisitor(SecretTaintedNodesMap &taintedNodesMap);
+
+  /// Compares each identifier in changedMap with the identifiers in baseMap. Returns the identifiers that either have
+  /// a different value or do not exist in baseMap at all.
+  /// \param baseMap The base map to compare the identifiers from changedMap with.
+  /// \param changedMap The changed map whose identifiers are compared with those provided by baseMap.
+  /// \return (A VariableValueMap) that contains all identifiers with a changed value or are not present in baseMap.
+  static VariableValueMap getChangedVariables(const VariableValueMap &baseMap, const VariableValueMap &changedMap);
 
   void visit(If &node) override;
 
@@ -38,16 +55,11 @@ class SpecialSecretBranchingVisitor : public ScopedVisitor {
 
   void visit(Assignment &elem) override;
 
-  static VariableValueMap getChangedVariables(const VariableValueMap &baseMap, const VariableValueMap &changedMap);
-
-  void addIdentifiers(Scope &scope);
-
   void visit(VariableDeclaration &node) override;
 
   void visit(FunctionParameter &node) override;
 
   void visit(Block &node) override;
-
 };
 
 #endif //GRAPHNODE_H_SRC_VISITOR_SECRETBRANCHINGVISITOR_H_
