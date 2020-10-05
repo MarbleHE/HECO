6acc1bdf77360c30fc99e513164211b130539e0d
Patrick Jattke <pjattke@ethz.ch>
2020-10-05 10:20:23 +0200

    Complements code documentation
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/Runtime/AbstractCiphertextFactory.h
+++ include/ast_opt/visitor/Runtime/AbstractCiphertextFactory.h
@@ -8,8 +8,21 @@ class AbstractCiphertext;
 
 class AbstractCiphertextFactory {
  public:
+  /// Creates a new AbstractCiphertext with the instantiated concrete ciphertext factory. The last value is used to fill
+  /// up the remaining slots of the ciphertext, in case that the given data does not use all slots.
+  /// \param data The data that should be encrypted in the ciphertext.
+  /// \return (A std::unique_ptr) to the created AbstractCiphertext.
   virtual std::unique_ptr<AbstractCiphertext> createCiphertext(std::vector<int64_t> &data) = 0;
 
+  /// Creates a new AbstractCiphertext with the instantiated concrete ciphertext factory. The given value is written
+  /// into each slot of the ciphertext.
+  /// \param data The value that should be encrypted in the ciphertext.
+  /// \return (A std::unique_ptr) to the created AbstractCiphertext.
+  virtual std::unique_ptr<AbstractCiphertext> createCiphertext(int64_t data) = 0;
+
+  /// Decrypts a given Abstractciphertext and writes the decrypted and decoded results into the given vector reference.
+  /// \param abstractCiphertext (A reference to) the ciphertext that should be decrypted and decoded.
+  /// \param ciphertextData (A reference to) the vector where the decrypted and decoded values are written to.
   virtual void decryptCiphertext(AbstractCiphertext &abstractCiphertext, std::vector<int64_t> &ciphertextData) = 0;
 };
 
