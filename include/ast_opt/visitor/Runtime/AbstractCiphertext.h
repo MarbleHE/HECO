35983c8bb53514a635c5e9ebc71af23638ca5c03
Patrick Jattke <pjattke@ethz.ch>
2020-10-06 18:32:47 +0200

    Adds [[nodiscard]] specifiers to non in-place methods
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/Runtime/AbstractCiphertext.h
+++ include/ast_opt/visitor/Runtime/AbstractCiphertext.h
@@ -12,7 +12,7 @@ class AbstractCiphertext {
   /// Multiplies this ciphertext with the given ciphertext and returns a new ciphertext with the result.
   /// \param operand The ciphertext to multiply this ciphertext with.
   /// \return A std::unique_ptr<AbstractCiphertext> containing the result ciphertext.
-  virtual std::unique_ptr<AbstractCiphertext> multiply(AbstractCiphertext &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> multiply(AbstractCiphertext &operand) = 0;
 
   /// Multiplies this ciphertext with the given ciphertext by overwriting this ciphertext with the result.
   /// \param operand The ciphertext to multiply this ciphertext with.
@@ -21,7 +21,7 @@ class AbstractCiphertext {
   /// Multiplies this ciphertext with the given ExpressionList, that is, performs a ciphertext-plaintext multiplication.
   /// \param operand The operand that should be multiplied with this ciphertext.
   /// \return (A unique_ptr to) the resulting product of the multiplication.
-  virtual std::unique_ptr<AbstractCiphertext> multiplyPlain(ExpressionList &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> multiplyPlain(ExpressionList &operand) = 0;
 
   /// Multiplies this ciphertext with the given ExpressionList by overwriting this ciphertext with the result, that is,
   /// performs an in-place ciphertext-plaintext multiplication.
@@ -31,7 +31,7 @@ class AbstractCiphertext {
   /// Adds this ciphertext to the given ciphertext and returns a new ciphertext with the result.
   /// \param operand The ciphertext to add to this ciphertext.
   /// \return A std::unique_ptr<AbstractCiphertext> containing the result ciphertext.
-  virtual std::unique_ptr<AbstractCiphertext> add(AbstractCiphertext &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> add(AbstractCiphertext &operand) = 0;
 
   /// Adds this ciphertext to the given ciphertext by overwriting this ciphertext with the result.
   /// \param operand The ciphertext to add to this ciphertext.
@@ -40,7 +40,7 @@ class AbstractCiphertext {
   /// Adds this ciphertext to the given ExpressionList, that is, performs a ciphertext-plaintext addition.
   /// \param operand The operand that should be added to this ciphertext.
   /// \return (A unique_ptr to) the resulting sum of the addition.
-  virtual std::unique_ptr<AbstractCiphertext> addPlain(ExpressionList &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> addPlain(ExpressionList &operand) = 0;
 
   /// Adds this ciphertext to the given ExpressionList by overwriting this ciphertext with this result, that is,
   /// performs an in-place ciphertext-plaintext addition.
@@ -50,7 +50,7 @@ class AbstractCiphertext {
   /// Subtracts the given ciphertext from this ciphertext and returns a new ciphertext with the result.
   /// \param operand The ciphertext to add to this ciphertext.
   /// \return A std::unique_ptr<AbstractCiphertext> containing the result ciphertext.
-  virtual std::unique_ptr<AbstractCiphertext> subtract(AbstractCiphertext &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> subtract(AbstractCiphertext &operand) = 0;
 
   /// Subtracts the given ciphertext from this given ciphertext by overwriting this ciphertext with the result.
   /// \param operand The ciphertext to subtract from this ciphertext.
@@ -59,7 +59,7 @@ class AbstractCiphertext {
   /// Subtracts the given ExpressionList from this ciphertext, that is, performs a ciphertext-plaintext subtraction.
   /// \param operand The operand that should be subtracted from this ciphertext.
   /// \return (A unique_ptr to) the result of the subtraction.
-  virtual std::unique_ptr<AbstractCiphertext> subtractPlain(ExpressionList &operand) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> subtractPlain(ExpressionList &operand) = 0;
 
   /// Subtracts the given ExpressionList from this ciphertext by overwriting this ciphertext with the result, that is,
   /// performs an in-place ciphertext-plaintext addition.
@@ -69,7 +69,7 @@ class AbstractCiphertext {
   /// Cyclically rotates a copy of this ciphertext by the given number of steps and returns the rotated ciphertext.
   /// \param steps The number of steps a copy of this ciphertext should be rotated.
   /// \return A std::unique_ptr<AbstractCiphertext> containing the rotated ciphertext.
-  virtual std::unique_ptr<AbstractCiphertext> rotateRows(int steps) = 0;
+  [[nodiscard]] virtual std::unique_ptr<AbstractCiphertext> rotateRows(int steps) = 0;
 
   /// Cyclically rotates this ciphertext by the given number of steps.
   /// \param steps The number of steps this ciphertext should be rotated.
