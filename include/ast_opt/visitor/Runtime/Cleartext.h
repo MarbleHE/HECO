10b13cffb5ed1499899e4c28e023a608cf06d14f
Patrick Jattke <pjattke@ethz.ch>
2020-10-07 23:52:05 +0200

    Extends Cleartext class
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/Runtime/Cleartext.h
+++ include/ast_opt/visitor/Runtime/Cleartext.h
@@ -4,14 +4,49 @@
 #include <vector>
 #include <cstdint>
 
+#include "ast_opt/ast/Literal.h"
 #include "ast_opt/visitor/Runtime/AbstractValue.h"
+#include "ast_opt/ast/ExpressionList.h"
 
+template<typename T>
 class Cleartext : public AbstractValue {
  private:
-  // the value this cleartext holds, currently only integers are supported as SEAL-BFV can only handle integers
-  std::vector<int64_t> data;
+  std::vector<T> data;
+
  public:
-  
+  explicit Cleartext(Literal<T> &node);
+
+  explicit Cleartext(ExpressionList &node);
+
+  const std::vector<T> &getData() const;
 };
+
+template<typename>
+struct isCleartext : public std::false_type {};
+
+template<typename T>
+struct isCleartext<Cleartext<T>> : public std::true_type {};
+
+template<typename T>
+Cleartext<T>::Cleartext(Literal<T> &node) {
+  data.push_back(node.getValue());
+}
+
+template<typename T>
+Cleartext<T>::Cleartext(ExpressionList &node) {
+  auto expressions = node.getExpressions();
+  for (auto &expression : expressions) {
+    if (auto exprCasted = dynamic_cast<Literal<T> *>(&expression.get())) {
+      data.push_back(exprCasted->getValue());
+    } else {
+      throw std::runtime_error("");
+    }
+  }
+}
+
+template<typename T>
+const std::vector<T> &Cleartext<T>::getData() const {
+  return data;
+}
 
 #endif //GRAPHNODE_H_INCLUDE_AST_OPT_VISITOR_RUNTIME_CLEARTEXT_H_
