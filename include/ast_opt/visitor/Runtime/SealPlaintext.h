79d46c890e60d6b130c714056c52ca9e862c2792
Patrick Jattke <pjattke@ethz.ch>
2020-10-08 11:57:15 +0200

    Implements basic methods for SealPlaintext
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/Runtime/SealPlaintext.h
+++ include/ast_opt/visitor/Runtime/SealPlaintext.h
@@ -2,10 +2,29 @@
 #define GRAPHNODE_H_INCLUDE_AST_OPT_VISITOR_RUNTIME_SEALPLAINTEXT_H_
 
 #include "ast_opt/visitor/Runtime/AbstractPlaintext.h"
+#include "SealCiphertextFactory.h"
+
+#ifdef HAVE_SEAL_BFV
+#include <seal/seal.h>
 
 class SealPlaintext : public AbstractPlaintext {
  private:
+  /// The encoded data in this plaintext wrapper.
+  seal::Plaintext plaintext;
+
+  /// A reference to the factory that created this ciphertext.
+  SealCiphertextFactory &factory;
+
  public:
+  ~SealPlaintext() override = default;
+
+  SealPlaintext(SealCiphertextFactory &sealFactory);
+
+  [[nodiscard]] const seal::Plaintext &getPlaintext() const;
+
+  seal::Plaintext &getPlaintext();
 };
+
+#endif
 
 #endif //GRAPHNODE_H_INCLUDE_AST_OPT_VISITOR_RUNTIME_SEALPLAINTEXT_H_
--- src/visitor/Runtime/SealPlaintext.cpp
+++ src/visitor/Runtime/SealPlaintext.cpp
@@ -1,1 +1,15 @@
 #include "ast_opt/visitor/Runtime/SealPlaintext.h"
+
+#ifdef HAVE_SEAL_BFV
+
+const seal::Plaintext &SealPlaintext::getPlaintext() const {
+  return plaintext;
+}
+
+SealPlaintext::SealPlaintext(SealCiphertextFactory &sealFactory) : factory(sealFactory) {}
+
+seal::Plaintext &SealPlaintext::getPlaintext() {
+  return plaintext;
+}
+
+#endif
