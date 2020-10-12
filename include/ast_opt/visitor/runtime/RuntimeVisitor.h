2c648674b1414e45f88d8e5fa49d7f63d81078d7
Patrick Jattke <pjattke@ethz.ch>
2020-10-12 12:39:59 +0200

    Adds docs to RuntimeVisitor's header file
    Signed-off-by: Patrick Jattke <pjattke@ethz.ch>

--- include/ast_opt/visitor/runtime/RuntimeVisitor.h
+++ include/ast_opt/visitor/runtime/RuntimeVisitor.h
@@ -29,24 +29,33 @@ typedef std::vector<std::pair<std::string, std::unique_ptr<AbstractValue>>> Outp
 
 class SpecialRuntimeVisitor : public ScopedVisitor {
  private:
-  ///
+  /// A stack that keeps track of intermediate results. Each visit(..) of an expression (node that inherits from
+  /// AbstractExpression) pushes its evaluation result on the stack so that the parent node can acccess the result.
   std::stack<std::unique_ptr<AbstractValue>> intermedResult;
 
-  ///
+  /// A map that provides datatype information for each scoped identifier.
   VariableDatatypeMap identifierDatatypes;
 
-  ///
+  /// A map that keeps track of ciphertexts that are created for declarations of secret variables.
   VariableMap<std::unique_ptr<AbstractCiphertext>> declaredCiphertexts;
 
+  /// A map that keeps track of declared non-secret variables (i.e., cleartexts).
   VariableMap<std::unique_ptr<ICleartext>> declaredCleartexts;
 
-  ///
+  /// A reference to the factory to be used to create ciphertexts in the given input program AST. This is required as
+  /// we need to create new ciphertexts for secret variable declarations.
   AbstractCiphertextFactory &factory;
 
-  ///
+  /// A map that contains the unique node ID for each secret tainted node, i.e., a node that must be secret because any
+  /// of its dependant nodes (e.g., operand of binary expression) are secret.
   SecretTaintedNodesMap &secretTaintedMap;
 
  public:
+  /// Create a new SpecialRuntimeVisitor.
+  /// \param factory A reference to the factory that should be used to create the ciphertexts.
+  /// \param inputs The inputs of the program that should be executed with this SpecialRuntimeVisitor
+  /// (by calling executeAst).
+  /// \param secretTaintedNodesMap A map that contains the secret tainted nodes.
   SpecialRuntimeVisitor(AbstractCiphertextFactory &factory,
                         AbstractNode &inputs,
                         SecretTaintedNodesMap &secretTaintedNodesMap);
@@ -65,9 +74,14 @@ class SpecialRuntimeVisitor : public ScopedVisitor {
 
   void visit(IndexAccess &elem) override;
 
+  /// A template method that is used to avoid code duplication in the visit methods for Literal<T>. As they do not have
+  /// a common base type, we cannot create a single visit method for them.
+  /// \tparam T The Literal type that is visited.
+  /// \param elem (A reference) to the currently visiting Literal<T>.
   template<typename T>
   void visitHelper(Literal<T> &elem) {
-    intermedResult.emplace(std::make_unique<Cleartext<T>>(elem));
+    intermedResult.emplace(std::make_unique<Cleartext<T>>
+                               (elem));
   }
 
   void visit(LiteralBool &elem) override;
@@ -94,15 +108,33 @@ class SpecialRuntimeVisitor : public ScopedVisitor {
 
   void visit(Variable &elem) override;
 
+  /// Gets the top value of the stack containing intermediate results and removes it from the stack.
+  /// \return (A std::unique_ptr of) the most recently pushed intermediate result.
   std::unique_ptr<AbstractValue> getNextStackElement();
 
-  [[maybe_unused]] [[maybe_unused]] void printOutput(AbstractNode &outputAst, std::ostream &targetStream = std::cout);
+  /// Prints the output of the given ast (outputAst) to the given stream (targetStream).
+  /// \param outputAst The root node of the output AST, i.e., a single block with one or multiple assignments.
+  /// \param targetStream The stream to write the textual representation of the AST to.
+  [[maybe_unused]] void printOutput(AbstractNode &outputAst, std::ostream &targetStream = std::cout);
 
+  /// Verifies that the structure of the input and output AST meet the expected form. More precisely,
+  ///     * the input AST must consists of a single block with declaration statements,
+  ///     * and the output AST of a single block with assignment statements.
+  /// \tparam T The expected type of the statements in the block.
+  /// \param astRootNode The root node of the AST to check.
   template<typename T>
   void checkAstStructure(AbstractNode &astRootNode);
 
+  /// Gets the output specified by the given output AST.
+  /// \param outputAst The root node of the output AST that describes the program's return values.
+  /// \return (A copy of) a vector of pairs consisting of identifier (std::string) and the corresponding value
+  /// (std::unique_ptr<AbstractValue>).
   OutputIdentifierValuePairs getOutput(AbstractNode &outputAst);
 
+  /// Executes an input program given as AST. *NOTE*: As our RuntimeVisitor does not handle Functions yet, the visitor
+  /// must be called on the function's Block. The missing function signature (input and outputs args) are derived from
+  /// the input and output AST given in the constructor and the getOutput method, respectively.
+  /// \param rootNode The root node of the input program.
   void executeAst(AbstractNode &rootNode);
 };
 
