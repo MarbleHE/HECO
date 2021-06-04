f6b7ef6df25751916c14ba1c8e4170c3bc8c61a2
Patrick Jattke <pjattke@ethz.ch>
2021-06-04 09:16:18 +0200

    Adds rudimentary arg parsing

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,11 +1,13 @@
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 #include "main.h"
 #include "ast_opt/ast/Literal.h"
 #include "ast_opt/ast/Variable.h"
 #include "ast_opt/ast/Assignment.h"
 
-int main() {
+int main(int argc, char** argv) {
 
   // Create Literal nodes directly
   Literal<bool> a(false);
@@ -20,6 +22,30 @@ int main() {
 
   // Create a VariableAssignment node (boo = false;)
   Assignment va(std::make_unique<Variable>("boo"), std::make_unique<Literal<bool>>(false));
+
+  if (argc <= 1) {
+    std::cerr << "Program ast_demo called with illegal arguments! Expected: ./ast_demo <benchmark_name> where "
+              << "<benchmark_name> is any of: demo"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string benchmark_name = argv[1];
+  if (benchmark_name == "demo") {
+    std::ofstream file;
+    file.open("demo_values.csv");
+
+    // TODO add code for running demo benchmark
+    // for demonstration, we just write some random values (from SoK CSV) into a file here instead
+    file << "t_keygen,t_input_encryption,t_computation,t_decryption\n"
+              << "415,1117,38315,37"
+              << std::endl;
+
+    file.close();
+  } else {
+    std::cerr << "Given benchmark name " << benchmark_name << " is not valid." << std::endl;
+    return EXIT_FAILURE;
+  }
 }
 
 
