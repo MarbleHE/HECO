8718c2a395959d5553f603a7a1855785d93d4dbf
Patrick Jattke <pjattke@ethz.ch>
2021-06-05 10:29:08 +0200

    More fixes for plotting workflow

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-upload_file() {
-    TARGET_DIR=$1
-    shift
-    for item in "$@"; do
-        aws s3 cp $item ${S3_URL}/${S3_FOLDER}/${TARGET_DIR}/
-    done  
-}
-
-# we checkout and build the repo in the entrypoint script because otherwise we would have to rebuild the
-# dockerimage each time we update the code
-
 # check out the ABC repo
-# TODO: remove 'git checkout' after debugging
-git clone https://github.com/MarbleHE/ABC.git && cd ABC && git checkout ec2-automation
+git clone https://github.com/MarbleHE/ABC.git /ABC \
+    && cd /ABC/Docker \
+    && chmod +x run_all.sh \
+    && ./run_all.sh
 
-# build ABC
-mkdir build && cd build && cmake .. && make -j$(nproc)
-
-# run the benchmark by passing the bench name as arg to program, then upload results to S3 bucket
-echo "Running ABC demo..."
-target_filename=demo_values.csv
-/ABC/build/ast_demo $1 $target_filename \
-    && upload_file demo $target_filename
