a5b64d490cf90748634f78c5ded7e2f31f5752f1
Alexander Viand <alexander.viand@inf.ethz.ch>
2021-06-16 10:45:03 +0200

    change PrintVisitor to be based on PlainVisitor since it does not use Scopes

--- src/visitor/PrintVisitor.cpp
+++ src/visitor/PrintVisitor.cpp
@@ -20,11 +20,11 @@ void SpecialPrintVisitor::visit(AbstractNode &elem) {
   std::string curNodeString = elem.toString(false);
 
   // Output current node at required indentation
-  os << "NODE VISITED: " << getIndentation() <<curNodeString;
+  os << "NODE VISITED: " << getIndentation() << curNodeString;
 
   // increment indentation level and visit children, decrement afterwards
   ++indentation_level;
-  for(AbstractNode &c: elem) {
+  for (AbstractNode &c: elem) {
     c.accept(*this);
   }
   --indentation_level;
