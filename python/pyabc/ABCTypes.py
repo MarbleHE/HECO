62ded0776689ea6a7dae777ecb498d26a4c04ceb
Miro Haller <mihaller@student.ethz.ch>
2021-09-22 15:02:19 +0200

    Fix smaller issues found on Ubuntu docker image

--- python/pyabc/ABCTypes.py
+++ python/pyabc/ABCTypes.py
@@ -1,39 +1,38 @@
-
-from typing import TypeVar, NewType
+from typing import TypeVar, NewType, List
 
 # Generic types. Those rely on Python not enforcing types.
-Secret      = TypeVar("Secret")
-NonSecret   = TypeVar("NonSecret")
+Secret = TypeVar("Secret")
+NonSecret = TypeVar("NonSecret")
 
 # Specific types. Those are useful if we want to hint to the translation what value the arguments haves
-SecretInt       = NewType("SecretInt", int)
-SecretFloat     = NewType("SecretFloat", float)
-SecretDouble    = NewType("SecretDouble", float)
-SecretBool      = NewType("SecretBool", bool)
-SecretString    = NewType("SecretString", str)
-SecretChar      = NewType("SecretChar", str)
+SecretInt = NewType("SecretInt", int)
+SecretFloat = NewType("SecretFloat", float)
+SecretDouble = NewType("SecretDouble", float)
+SecretBool = NewType("SecretBool", bool)
+SecretString = NewType("SecretString", str)
+SecretChar = NewType("SecretChar", str)
 
-SecretIntVector     = NewType("SecretIntVector", list[int])
-SecretFloatVector   = NewType("SecretFloatVector", list[float])
-SecretDoubleVector  = NewType("SecretDoubleVector", list[float])
-SecretBoolVector    = NewType("SecretBoolVector", list[bool])
-SecretStringVector  = NewType("SecretStringVector", list[str])
-SecretCharVector    = NewType("SecretCharVector", list[str])
+SecretIntVector = NewType("SecretIntVector", List[int])
+SecretFloatVector = NewType("SecretFloatVector", List[float])
+SecretDoubleVector = NewType("SecretDoubleVector", List[float])
+SecretBoolVector = NewType("SecretBoolVector", List[bool])
+SecretStringVector = NewType("SecretStringVector", List[str])
+SecretCharVector = NewType("SecretCharVector", List[str])
 
-
-NonSecretInt    = NewType("NonSecretInt", int)
-NonSecretFloat  = NewType("NonSecretFloat", float)
+NonSecretInt = NewType("NonSecretInt", int)
+NonSecretFloat = NewType("NonSecretFloat", float)
 NonSecretDouble = NewType("NonSecretDouble", float)
-NonSecretBool   = NewType("NonSecretBool", bool)
+NonSecretBool = NewType("NonSecretBool", bool)
 NonSecretString = NewType("NonSecretString", str)
-NonSecretChar   = NewType("NonSecretChar", str)
+NonSecretChar = NewType("NonSecretChar", str)
 
-NonSecretIntVector      = NewType("NonSecretIntVector", list[int])
-NonSecretFloatVector    = NewType("NonSecretFloatVector", list[float])
-NonSecretDoubleVector   = NewType("NonSecretDoubleVector", list[float])
-NonSecretBoolVector     = NewType("NonSecretBoolVector", list[bool])
-NonSecretStringVector   = NewType("NonSecretStringVector", list[str])
-NonSecretCharVector     = NewType("NonSecretCharVector", list[str])
+NonSecretIntVector = NewType("NonSecretIntVector", List[int])
+NonSecretFloatVector = NewType("NonSecretFloatVector", List[float])
+NonSecretDoubleVector = NewType("NonSecretDoubleVector", List[float])
+NonSecretBoolVector = NewType("NonSecretBoolVector", List[bool])
+NonSecretStringVector = NewType("NonSecretStringVector", List[str])
+NonSecretCharVector = NewType("NonSecretCharVector", List[str])
+
 
 def is_secret(val_type):
     """
@@ -44,8 +43,10 @@ def is_secret(val_type):
     :return: false if it is a NonSecret* type (as type or string), true otherwise
     """
 
-    non_secret_types = [NonSecret, NonSecretInt, NonSecretFloat, NonSecretDouble, NonSecretBool, NonSecretString, NonSecretChar, NonSecretIntVector,
-                        NonSecretFloatVector, NonSecretDoubleVector, NonSecretBoolVector, NonSecretStringVector, NonSecretCharVector]
+    non_secret_types = [NonSecret, NonSecretInt, NonSecretFloat, NonSecretDouble, NonSecretBool, NonSecretString,
+                        NonSecretChar, NonSecretIntVector,
+                        NonSecretFloatVector, NonSecretDoubleVector, NonSecretBoolVector, NonSecretStringVector,
+                        NonSecretCharVector]
 
     # Case 1: type is given as string, check if it corresponds to the string version of a non_secret type
     if isinstance(val_type, str):
