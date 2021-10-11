2578a4a8043320c6e2b3deffe4b0d9e0bb29250f
Gyorgy Rethy <grethy@student.ethz.ch>
2021-10-11 11:58:34 +0200

    WIP: Port GxKernel example from porcupine

--- test/end-to-end/GxKernelTest.cpp
+++ test/end-to-end/GxKernelTest.cpp
@@ -209,9 +209,9 @@ std::vector<int64_t> encryptedBatchedGxKernel(
 
   timer.stopTimer(encTimer);
 
-  auto compTimer = timer.startTimer();
   // Create rotated copies of the image and multiply by weights
   // std::cout << "Applying Kernel" << std::endl;
+  auto compTimer = timer.startTimer();
   std::vector<seal::Ciphertext> rotated_img_ctxts(9, seal::Ciphertext(context));
   for (size_t i = 0; i < rotations.size(); ++i)
   {
@@ -238,9 +238,112 @@ std::vector<int64_t> encryptedBatchedGxKernel(
 
   timer.stopTimer(compTimer);
 
-  auto decTimer = timer.startTimer();
   // Decrypt & Return result
-  // std::cout << "Decrypting Result" << std::endl;
+  auto decTimer = timer.startTimer();
+  seal::Plaintext result_ptxt;
+  decryptor.decrypt(result_ctxt, result_ptxt);
+  std::vector<int64_t> result;
+  encoder.decode(result_ptxt, result);
+  timer.stopTimer(decTimer);
+
+  return result;
+}
+
+/// Encrypted GxKernel, ported from Porcupine, according to:
+/// Ciphertext gx(Ciphertext c0, int h, int w)
+///     Ciphertext c1 = rotate(c0, w)
+///     Ciphertext c2 = add(c0, c1)
+///     Ciphertext c3 = rotate(c2, -w)
+///     Ciphertext c4 = add(c2, c3)
+///     Ciphertext c5 = rotate(c4, 1)
+///     Ciphertext c6 = rotate(c4, -1)
+///     return sub(c5, c6)
+/// Currently, this requires the image vector to be n/2 long,
+/// so we don't run into issues with rotations.
+/// \param img Pixel (x,y) = (column, row) should be at position x*imgSize + y
+/// \param poly_modulus_degree FHE parameter, degree n of the polynomials
+/// \return transformed image
+std::vector<int64_t> encryptedBatchedGxKernel_Porcupine(
+        MultiTimer &timer, const std::vector<int> &img, size_t poly_modulus_degree)
+{
+  auto keygenTimer = timer.startTimer();
+
+  // Input Check
+  if (img.size() != poly_modulus_degree / 2)
+  {
+    std::cerr << "WARNING: BatchedGxKernel might be incorrect when img.size() does not match N/2." << std::endl;
+  }
+
+  /// Rotations for 3x3 Kernel
+  /// Offsets correspond to the different kernel positions
+  // Since the middle weights are zero, we can actually get rid of those rotations
+  int img_size = (int) std::sqrt(img.size());
+
+  // Context Setup
+  seal::EncryptionParameters parameters(seal::scheme_type::bfv);
+  parameters.set_poly_modulus_degree(poly_modulus_degree);
+  parameters.set_coeff_modulus(seal::CoeffModulus::BFVDefault(parameters.poly_modulus_degree()));
+  parameters.set_plain_modulus(seal::PlainModulus::Batching(parameters.poly_modulus_degree(), 60));
+  seal::SEALContext context(parameters);
+
+  /// Create keys
+  seal::KeyGenerator keygen(context);
+  seal::SecretKey secretKey = keygen.secret_key();
+  seal::PublicKey publicKey;
+  keygen.create_public_key(publicKey);
+  seal::GaloisKeys galoisKeys;
+  keygen.create_galois_keys(galoisKeys);
+
+  // Create helper objects
+  seal::BatchEncoder encoder(context);
+  seal::Encryptor encryptor(context, publicKey, secretKey);
+  seal::Decryptor decryptor(context, secretKey);
+  seal::Evaluator evaluator(context);
+
+  timer.stopTimer(keygenTimer);
+
+  // Encode & Encrypt the image
+  auto encTimer = timer.startTimer();
+  seal::Plaintext img_ptxt;
+  seal::Ciphertext img_ctxt;
+  encoder.encode(std::vector<uint64_t>(img.begin(), img.end()), img_ptxt);
+  encryptor.encrypt(img_ptxt, img_ctxt);
+
+  timer.stopTimer(encTimer);
+
+  // Compute new image
+  auto compTimer = timer.startTimer();
+  // Ciphertext c1 = rotate(c0, w)
+  seal::Ciphertext c1;
+  evaluator.rotate_rows(img_ctxt, img_size, galoisKeys, c1); // img_ctxt == c0
+  // Ciphertext c2 = add(c0, c1)
+  seal::Ciphertext c2;
+  evaluator.add(img_ctxt, c1, c2);
+  // evaluator.add(img_ctxt, c1, c1); // c1 == c2
+  // Ciphertext c3 = rotate(c2, -w)
+  seal::Ciphertext c3;
+  evaluator.rotate_rows(c2, -1 * img_size, galoisKeys, c3);
+  // evaluator.rotate_rows(c1, -1 * img_size, galoisKeys, img_ctxt); // img_ctxt == c3
+  // Ciphertext c4 = add(c2, c3)
+  seal::Ciphertext c4;
+  evaluator.add(c2, c3, c4);
+  // evaluator.add(c1, img_ctxt, c1); // c1 == c4
+  // Ciphertext c5 = rotate(c4, 1)
+  seal::Ciphertext c5;
+  evaluator.rotate_rows(c4, 1, galoisKeys, c5);
+  // evaluator.rotate_rows(c1, 1, galoisKeys, img_ctxt); // img_ctxt == c5
+  // Ciphertext c6 = rotate(c4, -1)
+  seal::Ciphertext c6;
+  evaluator.rotate_rows(c4, -1, galoisKeys, c6);
+  // evaluator.rotate_rows_inplace(c1, -1, galoisKeys); // c1 == c6
+  // return sub(c5, c6)
+  seal::Ciphertext result_ctxt;
+  evaluator.sub(c5, c6, result_ctxt);
+  // evaluator.sub(img_ctxt, c1, img_ctxt);
+  timer.stopTimer(compTimer);
+
+  // Decrypt & Return result
+  auto decTimer = timer.startTimer();
   seal::Plaintext result_ptxt;
   decryptor.decrypt(result_ctxt, result_ptxt);
   std::vector<int64_t> result;
@@ -298,9 +401,9 @@ class GxKernelTest : public ::testing::Test {  /* NOLINT (predictable sequence e
 
   void printMatrix(size_t size, std::vector<int> &matrix) {
     for (int64_t row = (int64_t) size - 1; row >= 0; --row) {
-      std::cout << matrix.at(0*size + row);
+      std::cout << std::setw(6) << matrix.at(0*size + row);
       for (size_t col = 1; col < size; ++col) {
-        std::cout << "\t" << matrix.at(col*size + row);
+        std::cout << " " << std::setw(6) << matrix.at(col*size + row);
       }
       std::cout << std::endl;
     }
@@ -326,6 +429,28 @@ TEST_F(GxKernelTest, FastGxKernel_BatchedEncryptedGxKernel_Equivalence) { /* NOL
   auto fast = fastGxKernel(img);
   //std::cout << "fast:" << std::endl;
   //printMatrix(size, fast);
+
+  EXPECT_EQ(fast, enc);
+}
+
+TEST_F(GxKernelTest, FastGxKernel_PorcupineEncryptedGxKernel_Equivalence) { /* NOLINT */
+  size_t poly_modulus_degree = 2 << 12;
+  size_t img_size = std::sqrt(poly_modulus_degree / 2);
+  std::vector<int> img;
+  GxKernelTest::getInputMatrix(img_size, img);
+  std::cout << "img:" << std::endl;
+  printMatrix(img_size, img);
+
+  MultiTimer dummy = MultiTimer();
+  auto encrypted = encryptedBatchedGxKernel_Porcupine(dummy, img, poly_modulus_degree);
+  encrypted.resize(img.size());
+  std::vector<int> enc(begin(encrypted), end(encrypted));
+  std::cout << "encrypted:" << std::endl;
+  printMatrix(img_size, enc);
+
+  auto fast = fastGxKernel(img);
+  std::cout << "fast:" << std::endl;
+  printMatrix(img_size, fast);
 
   EXPECT_EQ(fast, enc);
 }
--- test/end-to-end/GxKernelTest.h
+++ test/end-to-end/GxKernelTest.h
@@ -15,5 +15,22 @@
 std::vector<int64_t> encryptedBatchedGxKernel(
         MultiTimer &timer, const std::vector<int> &img, size_t poly_modulus_degree, bool encrypt_weights = false);
 
+/// Encrypted GxKernel, ported from Porcupine, according to:
+/// Ciphertext gx(Ciphertext c0, int h, int w)
+///     Ciphertext c1 = rotate(c0, w)
+///     Ciphertext c2 = add(c0, c1)
+///     Ciphertext c3 = rotate(c2, -w)
+///     Ciphertext c4 = add(c2, c3)
+///     Ciphertext c5 = rotate(c4, 1)
+///     Ciphertext c6 = rotate(c4, -1)
+///     return sub(c5, c6)
+/// Currently, this requires the image vector to be n/2 long,
+/// so we don't run into issues with rotations.
+/// \param img Pixel (x,y) = (column, row) should be at position x*imgSize + y
+/// \param poly_modulus_degree FHE parameter, degree n of the polynomials
+/// \return transformed image
+std::vector<int64_t> encryptedBatchedGxKernel_Porcupine(
+        MultiTimer &timer, const std::vector<int> &img, size_t poly_modulus_degree);
+
 #endif //HAVE_SEAL_BFV
 #endif //AST_OPTIMIZER_GXKERNELTEST_H
