575ba996bcd77b1fc1aed7682f84a1f95116c0cf
Alexander Viand <alexander.viand@inf.ethz.ch>
2021-10-13 00:31:26 +0200

    wip: n-ary-op conversion (replace addi with fhe_add)

--- mlir/abc-opt/Nary.cpp
+++ mlir/abc-opt/Nary.cpp
@@ -16,9 +16,17 @@ void NaryPass::runOnOperation() {
   IRRewriter rewriter(&getContext());
 
   //TODO: There's very likely a much better way to do this that's not this kind of manual walk!
+
+  // First, go and translate std.addi to abc.fhe_add
+  // TODO: Do the same for std.muli and abc.fhe_mul
   for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
-    for (auto &op: llvm::make_early_inc_range(f.body().getOps())) {
-      //TODO: Do stuff to the operations!
+    for (auto op: llvm::make_early_inc_range(f.body().getOps<AddIOp>())) {
+      rewriter.setInsertionPointAfter(op.getOperation());
+      llvm::SmallVector<Value, 2> summands = {op.lhs(), op.rhs()};
+      Value value = rewriter.create<abc::AddOp>(op.getLoc(), op.result().getType(), summands);
+      op.replaceAllUsesWith(value);
+      rewriter.eraseOp(op.getOperation());
+
     }
   }
 
