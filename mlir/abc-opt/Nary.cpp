01388c245b4bf73813a44609634324c80d8fd3db
Alexander Viand <alexander.viand@inf.ethz.ch>
2021-10-13 05:04:10 +0200

    extend nary to sub, mul

--- mlir/abc-opt/Nary.cpp
+++ mlir/abc-opt/Nary.cpp
@@ -25,6 +25,44 @@ void collapseAdd(abc::AddOp &op, IRRewriter &rewriter) {
   }
 }
 
+void collapseSub(abc::SubOp &op, IRRewriter &rewriter) {
+  for (auto &use: llvm::make_early_inc_range(op.output().getUses())) {
+    if (auto use_sub = llvm::dyn_cast<abc::SubOp>(*use.getOwner())) {
+      rewriter.setInsertionPointAfter(use_sub.getOperation());
+      llvm::SmallVector<Value, 4> new_summands;
+      for (auto s: use_sub.summand()) {
+        if (s!=op.output()) {
+          new_summands.push_back(s);
+        }
+      }
+      for (auto s: op.summand()) {
+        new_summands.push_back(s);
+      }
+      auto new_sub = rewriter.create<abc::SubOp>(use_sub->getLoc(), use_sub.output().getType(), new_summands);
+      use_sub.replaceAllUsesWith(new_sub.getOperation());
+    }
+  }
+}
+
+void collapseMul(abc::MulOp &op, IRRewriter &rewriter) {
+  for (auto &use: llvm::make_early_inc_range(op.output().getUses())) {
+    if (auto use_mul = llvm::dyn_cast<abc::MulOp>(*use.getOwner())) {
+      rewriter.setInsertionPointAfter(use_mul.getOperation());
+      llvm::SmallVector<Value, 4> new_summands;
+      for (auto s: use_mul.terms()) {
+        if (s!=op.output()) {
+          new_summands.push_back(s);
+        }
+      }
+      for (auto s: op.terms()) {
+        new_summands.push_back(s);
+      }
+      auto new_mul = rewriter.create<abc::MulOp>(use_mul->getLoc(), use_mul.output().getType(), new_summands);
+      use_mul.replaceAllUsesWith(new_mul.getOperation());
+    }
+  }
+}
+
 void NaryPass::runOnOperation() {
   ConversionTarget target(getContext());
   target.addLegalDialect<AffineDialect, StandardOpsDialect, tensor::TensorDialect, scf::SCFDialect>();
@@ -37,7 +75,6 @@ void NaryPass::runOnOperation() {
   //TODO: There's very likely a much better way to do this that's not this kind of manual walk!
 
   // First, go and translate std.addi to abc.fhe_add
-  // TODO: Do the same for std.muli and abc.fhe_mul
   for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
     for (auto op: llvm::make_early_inc_range(f.body().getOps<AddIOp>())) {
       rewriter.setInsertionPointAfter(op.getOperation());
@@ -49,11 +86,43 @@ void NaryPass::runOnOperation() {
     }
   }
 
+  for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
+    for (auto op: llvm::make_early_inc_range(f.body().getOps<SubIOp>())) {
+      rewriter.setInsertionPointAfter(op.getOperation());
+      llvm::SmallVector<Value, 2> summands = {op.lhs(), op.rhs()};
+      Value value = rewriter.create<abc::SubOp>(op.getLoc(), op.result().getType(), summands);
+      op.replaceAllUsesWith(value);
+      rewriter.eraseOp(op.getOperation());
+
+    }
+  }
+
+  for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
+    for (auto op: llvm::make_early_inc_range(f.body().getOps<MulIOp>())) {
+      rewriter.setInsertionPointAfter(op.getOperation());
+      llvm::SmallVector<Value, 2> summands = {op.lhs(), op.rhs()};
+      Value value = rewriter.create<abc::MulOp>(op.getLoc(), op.result().getType(), summands);
+      op.replaceAllUsesWith(value);
+      rewriter.eraseOp(op.getOperation());
+    }
+  }
+
   // Now, go through and actually start collapsing the operations!
-  // TODO: Do the same for std.muli and abc.fhe_mul
   for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
     for (auto op: llvm::make_early_inc_range(f.body().getOps<abc::AddOp>())) {
       collapseAdd(op, rewriter);
+    }
+  }
+
+  for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
+    for (auto op: llvm::make_early_inc_range(f.body().getOps<abc::SubOp>())) {
+      collapseSub(op, rewriter);
+    }
+  }
+
+  for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
+    for (auto op: llvm::make_early_inc_range(f.body().getOps<abc::MulOp>())) {
+      collapseMul(op, rewriter);
     }
   }
 
