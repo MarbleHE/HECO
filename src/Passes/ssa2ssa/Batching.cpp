be31ef4dee374e1b89e0c19b010dc0872bc0a610
Alexander Viand <alexander.viand@inf.ethz.ch>
2022-01-17 10:33:37 +0100

    added temporary debug output to batching

--- src/Passes/ssa2ssa/Batching.cpp
+++ src/Passes/ssa2ssa/Batching.cpp
@@ -26,21 +26,37 @@ LogicalResult batchArithmeticOperation(IRRewriter &rewriter, MLIRContext *contex
   // We care only about ops that return scalars, assuming others are already "SIMD-compatible"
   if (auto result_st = op.getType().template dyn_cast_or_null<fhe::SecretType>()) {
 
-    llvm::outs() << "updating ";
-    op.print(llvm::outs());
-    llvm::outs() << "\n";
+    //llvm::outs() << "updating ";
+    //op.print(llvm::outs());
+    //llvm::outs() << "\n";
 
-    // Move rewriter
-    rewriter.setInsertionPointAfter(op);
 
     /// Target Slot (-1 => no target slot required)
     int target_slot = -1;
 
-    // convert all operands to batched
-    for (auto it = op->operand_begin(); it!=op.operand_end(); ++it) {
+//    /// List of used (Batched type) operand inputs' origins and the indices accessed in each one
+//    typedef llvm::SmallMapVector<Value, std::vector<int>, 1> OriginMap;
+//    OriginMap originMap;
+//
+//    auto addOriginUse = [&](Value o, int index) {
+//      if (originMap.find(o)!=originMap.end())
+//        originMap.find(o)->second.push_back(index);
+//      else
+//        originMap.insert({o, {index}});
+//    };
+
+    // new op with batched result type
+    auto bst = fhe::BatchedSecretType::get(context, result_st.getPlaintextType());
+    rewriter.setInsertionPointAfter(op);
+    auto new_op = rewriter.create<OpType>(op.getLoc(), bst, op->getOperands());
+    rewriter.setInsertionPoint(new_op); //otherwise, any operand transforming ops will be AFTER the new op
+
+    // convert all operands from scalar to batched and collect origin information
+    // CAREFUL: THIS ITERATES OVER THE ACTUAL OPERAND VALUES, WHILE WE NEED TO CHECK TYPE BASED ON WHAT THEY USED TO BE?
+    for (auto it = new_op->operand_begin(); it!=new_op.operand_end(); ++it) {
 
       if (auto bst = (*it).getType().template dyn_cast_or_null<fhe::BatchedSecretType>()) {
-        // already vector-style input, assume it's already slot-aligned appropriately
+        // already a vector-style input, no further action necessary
       } else if (auto st = (*it).getType().template dyn_cast_or_null<fhe::SecretType>()) {
         // scalar-type input that needs to be converted
         if (auto ex_op = (*it).template getDefiningOp<fhe::ExtractOp>()) {
@@ -50,31 +66,43 @@ LogicalResult batchArithmeticOperation(IRRewriter &rewriter, MLIRContext *contex
             target_slot = i; // we'll rotate by zero, but that's later canonicalized to no-op anyway
           auto rotate_op = rewriter
               .create<fhe::RotateOp>(ex_op.getLoc(), ex_op.vector(), i - target_slot);
-          rewriter.replaceOpWithIf(ex_op, {rotate_op}, [&](OpOperand &operand) { return operand.getOwner()==op; });
+          rewriter.replaceOpWithIf(ex_op, {rotate_op}, [&](OpOperand &operand) { return operand.getOwner()==new_op; });
+          //llvm::outs() << "rewritten operand in op: ";
+          //new_op.print(llvm::outs());
+          //llvm::outs() << '\n';
         } else if (auto c_op = (*it).template getDefiningOp<fhe::ConstOp>()) {
+          // Constant Ops don't take part in resolution?
           ShapedType shapedType = RankedTensorType::get({1}, c_op.value().getType());
           auto denseAttr = DenseElementsAttr::get(shapedType, ArrayRef<Attribute>(c_op.value()));
           auto new_cst = rewriter.template create<fhe::ConstOp>(c_op.getLoc(),
                                                                 fhe::BatchedSecretType::get(context,
                                                                                             st.getPlaintextType()),
                                                                 denseAttr);
-          rewriter.replaceOpWithIf(c_op, {new_cst}, [&](OpOperand &operand) { return operand.getOwner()==op; });
+          rewriter.replaceOpWithIf(c_op, {new_cst}, [&](OpOperand &operand) { return operand.getOwner()==new_op; });
         } else {
-          emitError(op.getLoc(), "Encountered unexpected defining op for secret operand while trying to batch.");
+          emitError(new_op.getLoc(),
+                    "Encountered unexpected (non batchable) defining op for secret operand while trying to batch.");
           return failure();
         }
 
       } else {
-        emitError(op.getLoc(), "Encountered unexpected non-secret operand while trying to batch.");
+        emitError(new_op.getLoc(), "Encountered unexpected non-secret operand while trying to batch.");
         return failure();
       }
     }
 
-    // new op with batched result type
-    auto bst = fhe::BatchedSecretType::get(context, result_st.getPlaintextType());
-    auto new_op = rewriter.create<OpType>(op.getLoc(), bst, op->getOperands());
+//    for (auto el: originMap) {
+//      llvm::outs() << "uses of: ";
+//      el.first.print(llvm::outs());
+//      llvm::outs() << " : ";
+//      for (auto i: el.second) {
+//        llvm::outs() << i << ", ";
+//      }
+//      llvm::outs() << '\n';
+//    }
 
     // Now create a scalar again by creating an extract, preserving type constraints
+    rewriter.setInsertionPointAfter(new_op);
     auto res_ex_op =
         rewriter.create<fhe::ExtractOp>(op.getLoc(),
                                         op.getType(),
@@ -84,6 +112,11 @@ LogicalResult batchArithmeticOperation(IRRewriter &rewriter, MLIRContext *contex
 
     // Finally, remove the original op
     rewriter.eraseOp(op);
+
+    //llvm::outs() << "current function: ";
+    //new_op->getParentOp()->print(llvm::outs());
+    //llvm::outs() << '\n';
+
   }
   return success();
 }
@@ -94,7 +127,6 @@ void BatchingPass::runOnOperation() {
   auto &block = getOperation()->getRegion(0).getBlocks().front();
   IRRewriter rewriter(&getContext());
 
-  //Translate Arithmetic Operations
   for (auto f: llvm::make_early_inc_range(block.getOps<FuncOp>())) {
     // We must translate in order of appearance for this to work, so we walk manually
     if (f.walk([&](Operation *op) {
@@ -107,6 +139,7 @@ void BatchingPass::runOnOperation() {
       if (auto mul_op = llvm::dyn_cast_or_null<fhe::MultiplyOp>(op))
         if (batchArithmeticOperation<fhe::MultiplyOp>(rewriter, &getContext(), mul_op).failed())
           return WalkResult::interrupt();
+      //TODO: Add support for relinearization!
       return WalkResult(success());
     }).wasInterrupted())
       signalPassFailure();
