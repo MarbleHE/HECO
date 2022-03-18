36f71b66cc3e7fefb82a0a09a34a9acb9095393e
Réthy György <rethygy0410@gmail.com>
2022-03-18 14:55:26 +0100

    [python-frontend] Add blueprint for unimplemented visitor and builder methods

--- python/pyabc/ABCMLIRTextBuilder.py
+++ python/pyabc/ABCMLIRTextBuilder.py
@@ -3,6 +3,7 @@ class ABCMLIRTextBuilder:
         self.indent_level = 0
         self.indent_string = " " * indent_size
         self.lines = list()
+        self.current_line = ""
 
     def _increase_indent(self):
         self.indent_level += 1
@@ -15,6 +16,7 @@ class ABCMLIRTextBuilder:
     def _add_line(self, string):
         append_string = f"{self.indent_string * self.indent_level}{string}"
         self.lines.append(append_string)
+        self.current_line = ""
 
     def dump(self):
         for line in self.lines:
@@ -31,4 +33,98 @@ class ABCMLIRTextBuilder:
     def add_end_function(self, func_obj):
         self._decrease_indent()
         self._add_line("}")
+        return self
+
+    def add_start_block(self, obj):
+        return self
+    
+    def add_end_block(self, obj):
+        return self
+
+    def add_start_variabledeclaration(self, obj):
+        print('add_start_variabledeclaration unimplemented')
+        return self
+
+    def add_end_variabledeclaration(self, obj):
+        print('add_end_variabledeclaration unimplemented')
+        return self
+
+    def add_start_variable(self, obj):
+        print('add_start_variable unimplemented')
+        return self
+
+    def add_end_variable(self, obj):
+        print('add_end_variable unimplemented')
+        return self
+
+    def add_start_literaldouble(self, obj):
+        print('add_start_literaldouble unimplemented')
+        return self
+
+    def add_end_literaldouble(self, obj):
+        print('add_end_literaldouble unimplemented')
+        return self
+
+    def add_start_void(self, obj):
+        print('add_start_void unimplemented')
+        return self
+
+    def add_end_void(self, obj):
+        print('add_end_void unimplemented')
+        return self
+
+    def add_start_if(self, obj):
+        print('add_start_if unimplemented')
+        return self
+
+    def add_end_if(self, obj):
+        print('add_end_if unimplemented')
+        return self
+
+    def add_start_binaryexpression(self, obj):
+        print('add_start_binaryexpression unimplemented')
+        return self
+
+    def add_end_binaryexpression(self, obj):
+        print('add_end_binaryexpression unimplemented')
+        return self
+
+    def add_start_return(self, obj):
+        print('add_start_return unimplemented')
+        return self
+
+    def add_end_return(self, obj):
+        print('add_end_return unimplemented')
+        return self
+
+    def add_start_literalint(self, obj):
+        print('add_start_literalint unimplemented')
+        return self
+
+    def add_end_literalint(self, obj):
+        print('add_end_literalint unimplemented')
+        return self
+
+    def add_start_int(self, obj):
+        print('add_start_int unimplemented')
+        return self
+
+    def add_end_int(self, obj):
+        print('add_end_int unimplemented')
+        return self
+
+    def add_start_for(self, obj):
+        print('add_start_for unimplemented')
+        return self
+
+    def add_end_for(self, obj):
+        print('add_end_for unimplemented')
+        return self
+
+    def add_start_assignment(self, obj):
+        print('add_start_assignment unimplemented')
+        return self
+
+    def add_end_assignment(self, obj):
+        print('add_end_assignment unimplemented')
         return self
--- python/pyabc/JSONVisitor.py
+++ python/pyabc/JSONVisitor.py
@@ -16,3 +16,77 @@ class JSONVisitor:
         builder = self.visit(obj["body"], builder)
         builder.add_end_function(obj)
         return builder
+
+    def visit_block(self, obj, builder):
+        builder.add_start_block(obj)
+        for stmt in obj["statements"]:
+            self.visit(stmt, builder)
+        builder.add_end_block(obj)
+        return builder
+
+    def visit_variabledeclaration(self, obj, builder):
+        builder.add_start_variabledeclaration(obj)
+        print('visit_variabledeclaration is unimplemented', obj)
+        builder.add_end_variabledeclaration(obj)
+        return builder
+
+    def visit_variable(self, obj, builder):
+        builder.add_start_variable(obj)
+        print('visit_variable is unimplemented', obj)
+        builder.add_end_variable(obj)
+        return builder
+
+    def visit_literaldouble(self, obj, builder):
+        builder.add_start_literaldouble(obj)
+        print('visit_literaldouble is unimplemented', obj)
+        builder.add_end_literaldouble(obj)
+        return builder
+
+    def visit_void(self, obj, builder):
+        builder.add_start_void(obj)
+        print('visit_void is unimplemented', obj)
+        builder.add_end_void(obj)
+        return builder
+
+    def visit_if(self, obj, builder):
+        builder.add_start_if(obj)
+        print('visit_if is unimplemented', obj)
+        builder.add_end_if(obj)
+        return builder
+
+    def visit_binaryexpression(self, obj, builder):
+        builder.add_start_binaryexpression(obj)
+        print('visit_binaryexpression is unimplemented', obj)
+        builder.add_end_binaryexpression(obj)
+        return builder
+
+    def visit_return(self, obj, builder):
+        builder.add_start_return(obj)
+        print('visit_return is unimplemented', obj)
+        builder.add_end_return(obj)
+        return builder
+
+    def visit_literalint(self, obj, builder):
+        builder.add_start_literalint(obj)
+        print('visit_literalint is unimplemented', obj)
+        builder.add_end_literalint(obj)
+        return builder
+
+    def visit_int(self, obj, builder):
+        builder.add_start_int(obj)
+        print('visit_int is unimplemented', obj)
+        builder.add_end_int(obj)
+        return builder
+
+    def visit_for(self, obj, builder):
+        builder.add_start_for(obj)
+        print('visit_for is unimplemented', obj)
+        builder.add_end_for(obj)
+        return builder
+
+    def visit_assignment(self, obj, builder):
+        builder.add_start_assignment(obj)
+        print('visit_assignment is unimplemented', obj)
+        builder.add_end_assignment(obj)
+        return builder
+
